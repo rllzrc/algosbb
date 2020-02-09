729b57fbdc21bb8129c540028fb8bd4cf7dac25d
rllzrc <rellas.email@gmail.com>
2020-02-09 16:53:20 -0500

    first commit, add recursion file

---
+++ 4semsOfCS/recursion.js
@@ -0,0 +1,18 @@
+// * basic example:
+
+function baesicRec(max, current) {
+  if(current > max) return;
+  return current;
+  basicRecursion(max, current+1);
+}
+
+// * classic recursion with fib!
+
+function fibonacci(n) {
+  // ---> can also write it out as if(n === 0 || n === 1) but for edge cases the n <= 2 is better..
+  if(n <= 2){ 
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+};
