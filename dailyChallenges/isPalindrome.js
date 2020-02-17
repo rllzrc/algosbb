7be0620384bc2a4ccb26b18c2bcaa3e52422e086
Rella <rellas.email@gmail.com>
2020-02-17 11:06:33 -0500

    add dailyChallenges folder

---
+++ dailyChallenges/isPalindrome.js
@@ -0,0 +1,18 @@
+// * create a function is Palindrome
+
+
+const isPalindrome = str => {
+  // iterate through string
+  // divide str in half and 'have pointers' checking from front to back
+  for(let i = 0; i < str.length/2; i++){
+    // check if first element is === to last
+    // use charAt with strings, cehck if not equal to make it easier
+    if(str.charAt(i) !== str.charAt([str.lenght-1-i])) {
+      return false;
+    }
+    return true;
+  }
+}
+
+console.log(isPalindrome('tacocat'));
+console.log(isPalindrome('hotdog'));4
