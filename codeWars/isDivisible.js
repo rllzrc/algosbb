0fa4d76372ded0e56393ba018bad817e3a11364c
Rella <rellas.email@gmail.com>
2020-02-23 18:07:10 -0500

    add isDivisible

--- codeWars/isDivisible.js
+++ codeWars/isDivisible.js
@@ -14,6 +14,12 @@ const isDivisible = (n,x,y) => {
   return false;
 }
 
+// isDivisible with ternary!
+
+function isDivisible(n, x, y) {
+  return (n % x === 0 && n % y === 0) ? true : false;
+}
+
 
 
 // test cases
