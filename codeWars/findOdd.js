82a3c9d3a8600adbfe917d9f86de1dfed6cfeb95
Rella <rellas.email@gmail.com>
2020-02-24 15:46:23 -0500

    finish findOdd

--- codeWars/findOdd.js
+++ codeWars/findOdd.js
@@ -16,25 +16,47 @@ function findOdd (arr) {
     cache[arr[i]] = counter += 1;
   }
   
-  console.log(cache);
+  //console.log(cache);
   
-  for (var property in object) {
-    if (object.hasOwnProperty(property)) {
-      // Do things here
-    }
-  }
+  // for (var property in object) {
+  //   if (object.hasOwnProperty(property)) {
+  //     // Do things here
+  //   }
+  // }
 
-  for(let key in object) {
-    if(object.hasOwnProperty(key))V{
+  // for(let key in object) {
+  //   if(object.hasOwnProperty(key))V{
       
+  //   }
+  // }
+  
+  const counterValues = Object.entries(cache);
+  //console.log('whoa', counterValues);
+  //counterValues.sort((a, b) => a - b);
+  
+  // * attempt with forEach ...
+  // counterValues.forEach((e, i) => {
+  //   //console.log('e', e);
+  //   //console.log('i', i);
+  //   if(e[1] > e[1]){
+  //     console.log('2nd element', e[1]);
+  //     return e[1];
+  //   }
+  // });
+
+  // create a variable to store result;
+  let result;
+  for(let i = 0; i < counterValues.length; i++){
+    if(counterValues[i] > counterValues[i + 1] || counterValues.length === 1){
+      //console.log('get itt:', counterValues[i]);
+      result = counterValues[i].shift();
     }
   }
-  // const counterValues = Object.values(cache);
-  // //console.log('whoa', counterValues);
-  // counterValues.sort((a, b) => a - b);
-  // console.log('yooo!:', counterValues)
 
-  // return counterValues.pop();
+  //console.log('yesssss!!!!:', result);
+  //console.log('yooo!:', counterValues)
+
+  return result;
 }
 
 // test cases!
@@ -42,3 +64,13 @@ console.log([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]); // 5);
 //console.log(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])); // -1);
 console.log(findOdd([20,1,1,2,2,3,3,5,5,4,20,4,5])); // 5);
 console.log(findOdd([10])); // 10);
+
+
+// for(let prop in counterValues) {
+    
+//   console.log('this is val:', counterValues[prop]);
+//   console.log('plus 1:', counterValues[prop + 1]);
+//   if(counterValues[prop] > counterValues[prop + 1]) {
+//     return counterValues[prop];
+//   }
+// }
