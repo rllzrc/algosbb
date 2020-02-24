0f92706200b3998c7c162b97daf5caac4341ebab
Rella <rellas.email@gmail.com>
2020-02-24 10:31:34 -0500

    add findOdds challenge

---
+++ codeWars/findOdd.js
@@ -0,0 +1,44 @@
+// * findOdd 
+// gieven an array, find the integer that appears an odd num of times >> 
+// there will always be one integer that appears an odd num of times
+function findOdd (arr) {
+  //happy coding!
+
+  // create a new variable to store how many times an element has been found in the arr 
+  const cache = {};
+
+  // create a counter variable
+  let counter = 0;
+
+  // iterate through the arr
+  for(let i = 0; i < arr.length; i += 1) {
+    // check if the current element is
+    cache[arr[i]] = counter += 1;
+  }
+  
+  console.log(cache);
+  
+  for (var property in object) {
+    if (object.hasOwnProperty(property)) {
+      // Do things here
+    }
+  }
+
+  for(let key in object) {
+    if(object.hasOwnProperty(key))V{
+      
+    }
+  }
+  // const counterValues = Object.values(cache);
+  // //console.log('whoa', counterValues);
+  // counterValues.sort((a, b) => a - b);
+  // console.log('yooo!:', counterValues)
+
+  // return counterValues.pop();
+}
+
+// test cases!
+console.log([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]); // 5);
+//console.log(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])); // -1);
+console.log(findOdd([20,1,1,2,2,3,3,5,5,4,20,4,5])); // 5);
+console.log(findOdd([10])); // 10);
