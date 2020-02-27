f837e0b4a05692f587674b7f4d28f0345cf6eae4
rllzrc <rellas.email@gmail.com>
2020-02-27 00:40:32 -0500

    add notes to rDupes

--- leet/removeDupes.js
+++ leet/removeDupes.js
@@ -69,6 +69,16 @@ const removeDuplicates = nums => {
 //   console.log('this is dupes:', dupes);
 // };
 
+
+// * additional notes:
+
+// Since the array is already sorted, we can keep two pointers ii and jj, where ii is the slow-runner while jj is the fast-runner. As long as nums[i] = nums[j]nums[i]=nums[j], we increment jj to skip the duplicate.
+
+// When we encounter nums[j] \neq nums[i]nums[j] 
+// 
+// ​	
+//  =nums[i], the duplicate run has ended so we must copy its value to nums[i + 1]nums[i+1]. ii is then incremented and we repeat the same process again until jj reaches the end of array.
+
 // * test cases!
 
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
