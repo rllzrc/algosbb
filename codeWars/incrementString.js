f0ea34cd3c6c3954befa787db25781db6fca65f8
rllzrc <rellas.email@gmail.com>
2020-02-29 01:07:39 -0500

    update incrementStr third pass

--- codeWars/incrementString.js
+++ codeWars/incrementString.js
@@ -11,9 +11,20 @@ const incrementString = str => {
 
   let newStr = str.toLowerCase().split('');
 
-  // create a counter variable to add numbers to the end of the array
-  let counter = 0;
+  // * create a counter variable to add numbers to the end of the array
   // might need to set counter as the last element number so you can increase it by one correctly 
+  let counter = 0;
+
+  // * create a function to check if string passed has leading zeroes
+  function leadingZero (str) {
+    let leadingZeroStr;
+
+    if (Number(newStr).toString() !== newStr) {
+      leadingZeroStr = newStr;
+    }
+
+    return leadingZeroStr;
+  }
 
   // * since item is passed in as a string, numbers will be coerced to that type value. use regex to find it..?
   function hasNumbers(t) {
@@ -23,36 +34,44 @@ const incrementString = str => {
 
     // * with ES6, use destructuring and slice to obtain last item in the newStr arr
     const [lastItem] = newStr.slice(-1);
-    //console.log('last element:', lastItem);
+    console.log('last element:', lastItem);
 
 
-    // * call function to check if str has numbers
+    // * call regex / hasNumbers function to check if str has numbers
     hasNumbers(lastItem);
+    leadingZero(newStr);
+    
+    console.log('leading zero str:', leadingZeroStr);
+    
+    // to check if the lastItem aka the str passed is empty
+    // console.log('last item found is a #', lastItem);
 
   // * loop through the string with .split() and convert to lowercase
   
-
     // if last item is not a number, add counter ++ aka 1,2,3...
     if(hasNumbers(lastItem) === true && lastItem !== '0' && lastItem !== '9') {
       counter = lastItem;
       counter ++;
-      console.log('yay increase counter:', counter);
-      console.log('last el in if:', lastItem)
+      //console.log('yay increase counter:', counter);
+      //console.log('last el in if:', lastItem)
       newStr.pop();
       let output = newStr.join('');
-      console.log('newstring:', output);
-      console.log(output + counter);
+      //console.log('newstring:', output);
+      //console.log(output + counter);
     } else if (hasNumbers(lastItem) === false) {
       counter ++;
       let output = newStr.join('');
-      console.log(output + counter);
+      //console.log(output + counter);
+    } else if (Number(newStr).toString() !== newStr) {
+      leadingZeroStr = newStr;
+    } else if (lastItem === undefined) {
+      return 1;
     }
-
     
-
-
-  
-
+    
+    // for(let i = 0; i < leadingZeroStr.length; i += 1) {
+    //   console.log('gotem in the loop', gotem);
+    // }
 
 
 
