e5ca656efbf08dc61c6adadf367af3c9e397819f
rllzrc <rellas.email@gmail.com>
2020-03-01 02:17:33 -0500

    add primeFibonacci

---
+++ dailyChallenges/primeFibonacci.js
@@ -0,0 +1,28 @@
+import { isPromise } from "q";
+
+// given a number, n, print all the numbers that are in the first n Fibonacci numbers.
+
+const primeFibonacci = num => {
+  // console.log(number);
+  
+  // create a new arr variable
+  let arr = [1,1];
+  // loop through up until num
+  for (let i = 2; i < number; i += 1) {
+    arr.push(arr[arr.length-1] + array[array.length-2]);
+  }
+  //console.log(array);
+  return arr.filter(e => isPromise(e));
+}
+
+const isPrime = num => {
+  if(num < 2) {
+    return false;
+  } 
+  for (let i = 0; i < num; i +=1) {
+    if(num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
