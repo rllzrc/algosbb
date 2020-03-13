377b10795a41410279f1ccacd1af498cb57179cc
Rella <rellas.email@gmail.com>
2020-03-12 22:06:30 -0400

    add unshift.js file

--- dailyRewind/myUnshift.js
+++ dailyRewind/myUnshift.js
@@ -5,25 +5,41 @@
 
 // Do not use the built-in .shift method for this problem!
 
+// * input: an array and a target val of any type
+// * output: return a new array with give target as first element of the array
+// * CANNOT use built in .shift method
+
+const myUnshift = (arr, target) => {
+  
+  // create a new const to store result arr in, initialize to an empty array
+  const resultArr = [];
+  resultArr.push(target);
+  for(let i = 0; i < arr.length; i += 1) {
+    resultArr.push(arr[i]);
+  }
+
+  return resultArr;
+}
+
 
 console.log(myUnshift([1, 2, 3], 0)); // => [0, 1, 2, 3]
 
 // test suites!
 
-describe('myUnshift', () => {
+// describe('myUnshift', () => {
 
-  it('is a function', () => {
-    expect(typeof myUnshift).toEqual('function');
-  });
+//   it('is a function', () => {
+//     expect(typeof myUnshift).toEqual('function');
+//   });
 
-  it('returns an array', () => {
-    let returnedValue = myUnshift([1, 2, 3], 0);
-    expect(Array.isArray(returnedValue)).toEqual(true);
-  });
+//   it('returns an array', () => {
+//     let returnedValue = myUnshift([1, 2, 3], 0);
+//     expect(Array.isArray(returnedValue)).toEqual(true);
+//   });
 
-  it('returns a new array with the value as the first index', () => {
-    let returnedValue = myUnshift(['bears', 'cows', 'ducks'], 'antelopes');
-    expect(returnedValue).toEqual(['antelopes', 'bears', 'cows', 'ducks']);
-  });
+//   it('returns a new array with the value as the first index', () => {
+//     let returnedValue = myUnshift(['bears', 'cows', 'ducks'], 'antelopes');
+//     expect(returnedValue).toEqual(['antelopes', 'bears', 'cows', 'ducks']);
+//   });
 
-});
+// });
