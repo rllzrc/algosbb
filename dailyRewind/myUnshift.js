2bedc7366d7274cced0dd344d92f790dd9e0b135
Rella <rellas.email@gmail.com>
2020-03-12 22:11:49 -0400

    update unshift file

--- dailyRewind/myUnshift.js
+++ dailyRewind/myUnshift.js
@@ -21,9 +21,22 @@ const myUnshift = (arr, target) => {
   return resultArr;
 }
 
+// second solution:
+
+const myUnshift2 = (arr, target) => {
+  // create new const arr with the target as the first element
+  const resultArr = [target];
+
+  // loop through original arr
+  arr.forEach((e) => {
+    resultArr.push(e);
+  });
+
+  return resultArr;
+}
 
 console.log(myUnshift([1, 2, 3], 0)); // => [0, 1, 2, 3]
-
+console.log(myUnshift2([5,6,7], 4)); // => [4,5,6,7]
 // test suites!
 
 // describe('myUnshift', () => {
