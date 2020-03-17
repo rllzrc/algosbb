c92c2904bbce7b8ac3cf2759e3a52d0b9fb5c80e
rllzrc <rellas.email@gmail.com>
2020-03-16 23:18:28 -0400

    update targetSum

--- algoExpert/twoSum.js
+++ algoExpert/twoSum.js
@@ -1,16 +1,39 @@
+// write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. if any two nums in the input array sum up to the target, return those in a new array. if not, return an empty array.
 
+// assume that there will be at most one pair of nums summing up to the target sum
 
-
-  function twoNumberSum(array, targetSum) {
+function twoNumberSum(array, targetSum) {
     // Write your code here.
-  }
-  
+    // write a test case if array is empty, return empty array
+    const output = [];
+    if(!array) {
+      return output;
+    }
+
+    // iterate through the array; checking the first element with i
+    for(let i = 0; i < array.length; i += 1) {
+      // loop again checking the first with the second element
+      for(let k = 0; k < array.length; k += 1) {
+        // check and see if i doesn't equal k, meaning its the next element
+        if(i !== k) {
+          if(array[i] + array[k] === targetSum) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+}
 
 
 
 
 
-  // Do not edit the line below.
-  exports.twoNumberSum = twoNumberSum;
+console.log(twoNumberSum([4,6,1], 5)); // --> [1,4]
+console.log(twoNumberSum([4,6], 10)); // --> [4,6]
+console.log(twoNumberSum([1,3,5], 7)); // --> [];
   
   
