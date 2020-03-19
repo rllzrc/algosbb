a0d306e9203f4d92026014d5ffb731260275348a
rllzrc <rellas.email@gmail.com>
2020-03-18 23:22:39 -0400

    refactor twoSum

--- algoExpert/twoSum.js
+++ algoExpert/twoSum.js
@@ -2,6 +2,8 @@
 
 // assume that there will be at most one pair of nums summing up to the target sum
 
+// * brute force first pass returning a boolean
+
 function twoNumberSum(array, targetSum) {
     // Write your code here.
     // write a test case if array is empty, return empty array
@@ -9,7 +11,6 @@ function twoNumberSum(array, targetSum) {
     if(!array) {
       return output;
     }
-
     // iterate through the array; checking the first element with i
     for(let i = 0; i < array.length; i += 1) {
       // loop again checking the first with the second element
@@ -18,48 +19,68 @@ function twoNumberSum(array, targetSum) {
         if(i !== k) {
           if(array[i] + array[k] === targetSum) {
             return true;
-          } else {
-            return false;
           }
         }
       }
     }
-
-    return true;
+    return false;
 }
 
 // solution 2:
 
-// const twoSum2 = (arr, target) => {
-//   for(let i = 0; i < arr.length; i += 1) {
-//     for(let k = i + 1; k < arr.length; k += 1) {
-//       if(arr[i] + arr[k] === target) return true;
-//     }
+// * if returning a boolean value
 
-//   }
-//   return false;
-// }
+const twoSum2 = (arr, target) => {
+  for(let i = 0; i < arr.length; i += 1) {
+    for(let k = i + 1; k < arr.length; k += 1) {
+      if(arr[i] + arr[k] === target) return true;
+    }
+  }
+  return false;
+}
 
+// * if returning an array containing the two elements that add up to the target
 
-// function twoNumberSum(array, targetSum) {
+const twoSum3 = (arr, target) => {
+  const output= [];
+  for(let i = 0; i < arr.length; i += 1) {
+    for(let k = i + 1; k < arr.length; k += 1) {
+      if(arr[i] + arr[k] === target) {
+        output.push(arr[i], arr[k]);
+      }
+    }
+  }
+  return output.sort((a,b) => a-b);
+}
+
+// * returning an array containing the two elements that add up to the target
+// * using two pointer variables
+// * nested loops
+// * checking if first and second variables === target
+// * setting those two elements in an array and returning it out
+
+function twoNumberSum(array, targetSum) {
 	
-// 	for (let i = 0; i < array.length; i++) {
-// 		const firstNum = array[i];
-//     for (let j = i + 1; j < array.length; j++) {
-// 			const secondNum = array[j];
-//       if (firstNum + secondNum === targetSum) {
-// 				return [firstNum, secondNum];
-// 			}
-//     }
+	for (let i = 0; i < array.length; i++) {
+		const firstNum = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+			const secondNum = array[j];
+      if (firstNum + secondNum === targetSum) {
+				return [firstNum, secondNum];
+			}
+    }
 		
-//   }
-// 	return [];
+  }
+	return [];
+}
 
-// }
+// * the more efficient approach
 
 
-console.log(twoNumberSum([4,6,1], 5)); // --> [1,4]
-console.log(twoNumberSum([4,6], 10)); // --> [4,6]
-console.log(twoNumberSum([1,3,5], 7)); // --> [];
-  
+
+console.log(twoSum3([4,6,1], 5)); // --> [1,4]
+console.log(twoSum3([4,6], 10)); // --> [4,6]
+console.log(twoSum3([1,3,5], 7)); // --> [];
+console.log(twoSum3([], 1)); // --> []
+console.log(twoSum3([4,6,1,-3], 3)); // --> [-3,6]
   
