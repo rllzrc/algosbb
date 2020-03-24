f1c77b93058e1f85a25cdb8eeee8a4fc2cd4e993
rllzrc <rellas.email@gmail.com>
2020-03-24 00:23:31 -0400

    add new folder into misc and map.js file

---
+++ misc./arrMethodsFromScratch.js/map.js
@@ -0,0 +1,27 @@
+// create array method map from sratuch
+
+const addTwo = (el) => {
+  return result = el + 2;
+}
+
+const addS = (el) => {
+  
+}
+
+const map = (arr, cb) => {
+  // * create a new var to store result in
+  let output = [];
+  // loop through original arr
+  for(let i = 0; i < arr.length; i +=1) {
+    // * invoke original cb on each element
+      // then push each value into output
+    output.push(cb(arr[i]));
+  }
+  return output;
+}
+
+
+
+// * test cases!
+console.log(map([1,2,3], addTwo)); 
+console.log(map(['pizza', 'at', 'screamer', 'yum'], ))
