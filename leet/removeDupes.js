426a0976defcd666971d75550f22b380f4ad0ac2
Rella <rellas.email@gmail.com>
2020-04-08 17:22:57 -0400

    update removeDupes with two pointer approach

--- leet/removeDupes.js
+++ leet/removeDupes.js
@@ -2,7 +2,7 @@
 
 // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-const removeDuplicates = nums => {
+const removeDuplicates1 = nums => {
   
   // create a temp variable to hold onto the previous num
   let temp = 0;
@@ -16,10 +16,10 @@ const removeDuplicates = nums => {
     // create another var to store the current element
     // then compare it to temp
     let currElement = nums[i]
-    console.log('this is current element:', currElement);
+    //console.log('this is current element:', currElement);
 
     // check to see if currElement is equal to the temp variable aka previous number
-    console.log('this is previous element:', temp);
+    //console.log('this is previous element:', temp);
     if(currElement === temp) {
       nums.splice(i, 1);
     }
@@ -27,64 +27,41 @@ const removeDuplicates = nums => {
     
   }
 
-  console.log('arr after splice:', nums);
+  //console.log('arr after splice:', nums);
   return nums.length;
 }
 
+// * second approach w count variable 
+const removeDuplicates2 = nums => {
 
-// brute force approach
+  // * edge case if empty nums array
+  if(nums.length === 0) return 0;
 
-// const removeDuplicates = function(nums) {
+  if(nums.length === 1) return 1;
 
-//   // create a temp variable to store current element to match with next element
-//   let temp = 0;
+  let count = 0;
+  for(let i = 0; i < nums.length; i += 1) {
+    if(nums[i] !== nums[i +1]) {
+      count++;
+    }
+  }
+  return count;
+}
 
-//   // create a dupes variable to store how many times the same num shows up
-//   let dupes = 0;
+// * two pointer approach
 
-//   console.log(nums.length);
-//   // iterate through the array
-//   for(let i = 0; i < nums.length; i += 1) {
-//     temp = nums[i + 1]
-//     //console.log('this is temp:', temp);
-//     if(temp === nums[i]) {
-      
-//       console.log('temp in the if:', temp);
-      
-//       console.log('nums[i]/current element', nums[i]);
-//       console.log('this is index:', i);
-//       console.log('nums b4 splice:', nums);
-//       //nums.splice(i, 1);
-//       console.log('nums after splice:', nums);
-//       // temp =  nums[i + 1];
-//       dupes += 1;
-//       nums.splice(i, dupes);
-//     }
+const removeDuplicates = nums => {
+  let index = 1;
 
-//     dupes = 0;
-//     console.log('temp after if:', temp);
-
-//   }
-//   //console.log('after splice length:', nums.length);
-//   console.log('this is dupes:', dupes);
-// };
-
-
-// * additional notes/java solution:
-
-// public int removeDuplicates(int[] nums) {
-//   if (nums.length == 0) return 0;
-//   int i = 0;
-//   for (int j = 1; j < nums.length; j++) {
-//       if (nums[j] != nums[i]) {
-//           i++;
-//           nums[i] = nums[j];
-//       }
-//   }
-//   return i + 1;
-// }
+  for(let i = 0; i < nums.length-1; i += 1) {
+    if(nums[i] !== nums[i + 1]) {
+      nums[index++] = nums[i + 1];
+    }
+  }
+  return index;
+}
 
 // * test cases!
 
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
-console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])); // --> 5
+console.log(removeDuplicates([1,1,2])); // --> 2
