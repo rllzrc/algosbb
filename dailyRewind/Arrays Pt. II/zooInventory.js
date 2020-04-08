850628f72c4d7bc19aa9a59b845901decffd9322
Rella <rellas.email@gmail.com>
2020-04-08 17:39:34 -0400

    add zI file approach using ternary

--- dailyRewind/Arrays Pt. II/zooInventory.js
+++ dailyRewind/Arrays Pt. II/zooInventory.js
@@ -3,34 +3,14 @@
 
 // zooInventory should return a new, flat array. Each element in the new array should be a sentence about each of the animals in the zoo.
 
-const zooInventory = arr => {
-  // create a new array to hold the finished sentences
+function zooInventory(zoo) {
   let sentences = [];
 
-  // loop through the entire zoo array
-  for (let i = 0; i < arr.length; i++) {
-
-    // store the current animal
-    let animal = arr[i];
-
-    // store the name of the current animal
-    let name = arr[0];
-
-    // store the species of the current animal
-    let species = arr[1][0];
-
-    // store the age of the current animal
-    let age = arr[1][1];
-
-    // use string interpolation and our well-named variables to contruct the sentence
-    let sentence = `${name} the ${species} is ${age}.`
-
-    // push the sentence into the sentences array
-    sentences.push(sentence);
+  for (let i = 0; i < zoo.length; i++) {
+    sentences.push(`${zoo[i][0]} the ${zoo[i][1][0]} is ${zoo[i][1][1]}.`)
   }
 
-  // return the sentences array after the for loop
-  return sentences
+  return sentences;
 }
 
 const myZoo = [
