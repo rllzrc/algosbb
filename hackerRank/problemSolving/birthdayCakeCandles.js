0f8957335d6fd023c95be9caf91a7c30df201482
rllzrc <rellas.email@gmail.com>
2020-04-09 23:40:30 -0400

    fix bug w bCC file, first pass working

--- hackerRank/problemSolving/birthdayCakeCandles.js
+++ hackerRank/problemSolving/birthdayCakeCandles.js
@@ -11,12 +11,12 @@
 const birthdayCakeCandles = arr => {
   // create a variable to track the highest num
   let count = 0;
+  
   // sort the array so its already in order
-  let sortedArr = arr.sort();
+  let sortedArr = arr.sort((a,b) => a-b);
 
   // create a variable to store largest num, this case it will be the last item in the arr.
-  let height = sortedArr.length-1;
-  //console.log('this is the last item in the arr:', height);
+  let height = sortedArr[arr.length-1];
 
   // loop through the array 
   for(let i = 0; i < arr.length; i += 1) {
@@ -28,6 +28,6 @@ const birthdayCakeCandles = arr => {
   return count;
 }
 
-
 // * test cases! 
 console.log(birthdayCakeCandles([3,2,1,3])); // --> 2
+console.log(birthdayCakeCandles([3,2,1,3,4,5,5,5])); // --> 3
