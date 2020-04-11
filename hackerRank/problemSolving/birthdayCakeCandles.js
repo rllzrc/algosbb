08daee7239fa09bdc3e6205de4dedf0180839e17
rllzrc <rellas.email@gmail.com>
2020-04-11 17:45:22 -0400

    update second pass for bCC challenge file

--- hackerRank/problemSolving/birthdayCakeCandles.js
+++ hackerRank/problemSolving/birthdayCakeCandles.js
@@ -11,7 +11,7 @@
 const birthdayCakeCandles = arr => {
   // create a variable to track the highest num
   let count = 0;
-  
+
   // sort the array so its already in order
   let sortedArr = arr.sort((a,b) => a-b);
 
@@ -21,12 +21,13 @@ const birthdayCakeCandles = arr => {
   // loop through the array 
   for(let i = 0; i < arr.length; i += 1) {
     if(arr[i] === height) {
-      // if the current element is equal to count, add 1
+      // if the current element is equal to height, add 1 to count
       count += 1;
     }
   }
   return count;
 }
+
 
 // * test cases! 
 console.log(birthdayCakeCandles([3,2,1,3])); // --> 2
