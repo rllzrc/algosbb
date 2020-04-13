0fbd4e310777ccbce1bf82524a6ee325ddae5882
rllzrc <rellas.email@gmail.com>
2020-04-13 13:47:09 -0400

    update maxSubarr chllng with recurison & dvd&conqr approach

---
+++ leet/30DaysOfCode/contiguous Array.js
@@ -0,0 +1,17 @@
+// * Day 12 of 30 Days of Code! --->   Contiguous Array !
+
+// TASK!
+
+// Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+// Input: [0,1]
+// Output: 2
+// Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+
+const findMaxLength = arr => {
+  
+}
+
+// * test cases!
+console.log(findMaxLength([0,1])); // --> 2
+console.log(findMaxLength([0,1,0])); // --> 2
--- leet/30DaysOfCode/maximumSubArray.js
+++ leet/30DaysOfCode/maximumSubArray.js
@@ -46,5 +46,65 @@ const maxSubArray2 = arr => {
   return maxSum === 0 ? Math.max(...arr) : maxSum;
 }
 
+// * additional recursive solution w comments using Divide And Conquer Approach: 
 
-console.log(maxSubArray2([-2,1,-3,4,-1,2,1,-5,4])); // ---> 6
+const maxSubArray3 = arr => {
+
+  // return results of the recursive func
+  return findMaxSumInArr(arr);
+
+  // this is the recursive function that will divide and conquer to find max sum from subarry of the arr passed in as a parameter
+  function findMaxSumInArr(arr) {
+    // BASE CASE~
+    // if there is only one item in the arr, simply return that value
+    if(arr.length === 1) {
+      return arr[0];
+    }
+
+    // if no items in the arr, return -Infinity (needs to be a valid number for the calculations to take place later) 
+    // in JS --> can only store numbers > -Infinity, this number will never be the max value
+    if(arr.length === 0) {
+      return -Infinity; 
+    }
+
+    // create and declare variables: zero-indexed length and midpoint
+    let length = arr.length-1;
+    let mid = Math.floor(length / 2);
+
+    // * math stuff here! DIVISION: recursively find max sum in the left and right sub arrays
+    let leftMaxSumSubArr = findMaxSumInArr(arr.slice(0, mid));
+    let rightMaxSumSubArr = findMaxSumInArr(arr.slice(mid + 1, length + 1));
+
+    // account for MERGING: division step above gives us max sum of left and right sides, but consider the possibility of having contiguous array that goes from left to right through midpoint 
+
+    // create and declare variables to record the maximum contiguous sums for both sides 
+    let leftMaxContigousSum = 0;
+    let rightMaxContigousSum = 0;
+  
+
+    // * on left side, find sum of contigous array and keep updated record of max value
+    // HEADS UP: in order to account for contigous arrays that traverse the midpoint, start search from the midpoint - 1 index and traverse left towards index 0. This direction guarantees that a contigous array traversing the midpoint will be able to add the midpoint itself and the right side's contigous arr --> see retun statement below 
+    for(let i = mid - 1, currContiguousSum = 0; i >= 0; i -= 1) {
+      currContiguousSum += arr[i];
+      leftMaxContigousSum = Math.max(leftMaxContigousSum, currContiguousSum);
+    }
+
+    // * on right side, find sum of contigous array and keep an updated record of the max value
+    // same as on the left side, to account for sub arrays that traverse the midpoint, start search from midpoint + 1 index and go towards the right to the end of the array
+    for(let i = mid + 1, currContiguousSum = 0; i <= length; i += 1) {
+      currContiguousSum += arr[i];
+      rightMaxContigousSum = Math.max(rightMaxContigousSum, currContiguousSum);
+    }
+
+    // * RETURN out the max sum from the current array: either left, right, or contigous sub arr traversing from left to right to midpoint
+    return Math.max(
+      // this is the max sum from a contigous subarray that traverses the midpoint:
+      leftMaxContigousSum + arr[mid] + rightMaxContigousSum,
+      // max sum from each side whether a single value or a contigous sum)
+      leftMaxSumSubArr,
+      rightMaxSumSubArr
+    );
+  }
+}
+
+console.log(maxSubArray3([-2,1,-3,4,-1,2,1,-5,4])); // ---> 6
