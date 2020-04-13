c7ce2cbf4d78a1ca4edc8bf5518f54de114046b3
rllzrc <rellas.email@gmail.com>
2020-04-13 19:46:28 -0400

    update sumTrnsctns chllng w iterative approach

--- dailyRewind/Objects Pt. I/lastFridayNight.js
+++ dailyRewind/Objects Pt. I/lastFridayNight.js
@@ -7,9 +7,24 @@
 
 // * first pass using map and reduce!
 
-const sumTransactions = arr => {
+const sumTransactions1 = arr => {
   // declare a variable to store total amount
   const sum = arr.map(el => el.amount).reduce((a,b) => a + b, 0);
+  return sum;
+}
+
+// * second attempt using iterative solution
+
+const sumTransactions = arr => {
+  // declare a variable to store total in
+  let sum = 0;
+
+  // iterate through the arr
+  for(let i = 0; i < arr.length; i += 1) {
+    // store current el in a varaible 
+    let transaction = arr[i]
+    sum += transaction.amount
+  }
   return sum;
 }
 
