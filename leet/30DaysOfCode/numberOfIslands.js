b6ecc4e1c0fc0d945c4fff90b2053a02f43a9108
rllzrc <rellas.email@gmail.com>
2020-04-18 23:57:00 -0400

    add numOfIsl chllng solution w recursion; first attempt w comments

--- leet/30DaysOfCode/numberOfIslands.js
+++ leet/30DaysOfCode/numberOfIslands.js
@@ -2,6 +2,32 @@
 
 // TASK!
 
+// Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+// *** Example 1:
+
+// Input:
+// 11110
+// 11010
+// 11000
+// 00000
+
+// Output: 1
+
+const testOne = [
+  [0, 1, 1, 1, 0],
+  [1, 1, 0, 1, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+​
+const testTwo = [
+  [1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 1, 1],
+];
+
 // * first pass using recursion!
 
 const numIslands = grid => {
@@ -31,3 +57,5 @@ const visitIsland = (i, j, grid) => {
   if (grid[i][j + 1] === "1") visitIsland(i, j + 1, grid);
   if (grid[i][j - 1] === "1") visitIsland(i, j - 1, grid);
 }
+
+console.log(numIslands(testOne)) // --> 1
