124fed29dd47d159763d5cf776f9a05c3f3f13da
rllzrc <rellas.email@gmail.com>
2020-05-02 18:41:07 -0400

    update quickSort file w additional comments/answers for chapter exercises

--- grokkingAlgos/quickSort.js
+++ grokkingAlgos/quickSort.js
@@ -54,16 +54,33 @@ console.log(maxNum([0, 1, 2, 3, 4, 5])); // 6
 
 // ********* QUICK SORT ~!
 
-const quickSort = arr => {
+const quickSort1 = arr => {
   // start with base case --> arrays with zero to 1 elements are already sorted!
   if(arr.length < 2) return arr;
 
   const pivot = arr[0]; 
+
+  // slice grabs first element on wards in arr, filter will check each element against pivot
   const lessThan = arr.slice(1).filter(e => e <= pivot);
   const greaterThan = arr.slice(1).filter(e => e > pivot);
 
+  // spread operator to concat arrays together without duplicates
   return [...quickSort(lessThan), pivot, ...quickSort(greaterThan)];
 
 }
 
 console.log(quickSort([10, 5, 2, 3])); // --> [2, 3, 5, 10]
+
+
+// ** additional exercises from the chapter:
+
+// HOW LONG would each of these OPERATIONS take in BIG O Notation?
+
+// 5. Printing the value of each element in the array.
+// * O(n) --> linear time
+// 6. Doubling the value of each element in an array.
+// * O(n) --> linear
+// 7. Doubling the value of just the first element in the array.
+// * O(1) --> constant 
+// 8. Creating a multiplication table with all of the elements in the array.
+// * O(n^2)
