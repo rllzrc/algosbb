8a94bd502f4099fa8b1732c3b44305ea5ed3a4ff
rllzrc <rellas.email@gmail.com>
2020-05-09 00:28:53 -0400

    update with recursive solution, refactor if statements to pass all test cases

--- Arrays!/plusOne.js
+++ Arrays!/plusOne.js
@@ -8,8 +8,7 @@
 // You may assume the integer does not contain any leading zero, except the number 0 itself.
 
 // * first attempt:
-
-const plusOne = digits => {
+const plusOne1 = digits => {
   // brute force approach:
 
   // iterate through the end of the array
@@ -23,6 +22,30 @@ const plusOne = digits => {
 
   return digits;
 }
+
+// * second attempt using recursion
+const plusOne = digits => {
+  // create a variable to store last index value
+  const lastIndex = digits.length-1;
+
+  // base case
+  if(digits.length === 0) {
+    digits[0] = 1;
+    return digits;
+  } 
+
+  // check if the last digit is a 9, change that value to 0 and run the function again with the same array minus the last digit while adding that 0
+  if(digits[lastIndex] === 9) {
+    digits[lastIndex] = 0;
+
+    // so slicing from the beginning to last and adding in the zero as its last value, making a new array
+    return plusOne(digits.slice(0, lastIndex).concat(digits[lastIndex]));
+  } else {
+    // if the last digit is not a 9, increment its value
+    digits[lastIndex] += 1;
+    return digits;
+  }
+};
 
 // * test cases!!
 console.log(plusOne([1,2,3])); // -> [1,2,4]
