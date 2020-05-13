ea1c7bf8e70c8f0a5663ded36a4715ba4f25b3e1
rllzrc <rellas.email@gmail.com>
2020-05-12 22:29:31 -0400

    update singleElInSortedArr solution #2 using binary search method O(log n) time

--- leet/30DaysOfCodePt2/singleElementInSortedArray.js
+++ leet/30DaysOfCodePt2/singleElementInSortedArray.js
@@ -10,7 +10,7 @@
 // * first attempt:
 // runtime complexity:
 // * O(n) --> linear, loop through each element once
-const singleNonDuplicate = nums => {
+const singleNonDuplicate1 = nums => {
   // create a cache variable
   const cache = {};
 
@@ -34,7 +34,59 @@ const singleNonDuplicate = nums => {
       return nums[i];
     }
   }
+
+}
+
+// thoughts on binary search method:
+// use two pointer approach L and R
+// find the middle value --> L + R / 2
+// check for same value (mid + 1 or mid - 1) will be that subarray split
+// use % to see if subarray halfs are even or odd (r-m) % 2 = 0
+// if mid pair is on the right and halves are even: left = mid + 2
+// if mid pair is on right and halves are odd: right = mid - 1
+// if mid pair on left and halves are even: right = mid - 2
+// if pair is on left and halves are odd: left = mid + 1
+
+// * second attempt: using binary search
+// runtime complexity:
+// * O(log n)
+// space complexity:
+// * O(1)
+const singleNonDuplicate = nums => {
+  // create two pointer variables: left and right
+  let left = 0;
+  let right = nums.length-1;
+
+  // loop through while left is less than right
+  while(left < right) {
+    // create a mid variable to split num arr in half
+    let mid = (left + right) / 2;
+    // check if halves are even; so once it is broken down into two subarrays:
+    let splitEven = (right - mid) % 2 === 0;
+    // upon split, if the matching pair of mid is on the right side and halves are even reassign left value
+    if(nums[mid + 1] === nums[mid]) {
+      if(splitEven) {
+        // reassign left value
+        left = mid + 2;
+      } else {
+        // if pair is on the right but its not even, reassign right value
+        right = mid - 1;
+      } 
+    } else if(nums[mid - 1] === nums[mid]) {
+      // after the split if the matching pair of mid is on the left and halves are even reassign right value
+      if(splitEven) {
+        right = mid - 2;
+      } else {
+        // if the pair is on the left but its not even, reassign left value
+        left = mid + 1;
+      }
+    } else {
+      // if it doesnt satisfy any of the above, simly return nums[mid] value
+      return nums[mid];
+    }
+  }
   
+  return nums[left];
 }
 
 // * test cases !!!
