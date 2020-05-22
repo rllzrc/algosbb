03ed7d5f3eb73059177c68778f148d8d6bec98ea
rllzrc <rellas.email@gmail.com>
2020-05-22 19:25:30 -0400

    update second solution to sortCharByFreq; refactor to clean and comment out code

--- leet/30DaysOfCodePt2/sortCharactersByFrequency.js
+++ leet/30DaysOfCodePt2/sortCharactersByFrequency.js
@@ -4,7 +4,7 @@
 // Given a string, sort it in decreasing order based on the frequency of characters.
 
 // * first attempt: use a cache object/hash map type of DS
-const frequencySort = s => {
+const frequencySort1 = s => {
   // initialize a cache object to keep track of number of frequencies 
   const cache = {};
 
@@ -37,7 +37,7 @@ const frequencySort = s => {
 }
 
 // * second attempt:
-var frequencySort = function(s) {
+var frequencySort3 = function(s) {
   const frequency = {}
   const result = []
   for (let i = 0; i < s.length; i++) {
@@ -54,11 +54,18 @@ var frequencySort = function(s) {
   return result.join('')
 };
 
-const frequencySort2 = s => {
+const frequencySort = s => {
   // create a cache to store values in
   const cache = {};
   // create a results array to return out later
   const resultArr = [];
+
+  // iterate through s input and map out characters into cache
+  for(let i = 0; i < s.length; i += 1) {
+    cache[s[i]] = (cache[s[i]] || 0) + 1;
+  }
+
+  //console.log(cache)
   
 }
 // * test cases!!!~
