190cc96b4b6d08f585d8bf0babffd0768e680609
rllzrc <rellas.email@gmail.com>
2020-05-25 21:07:40 -0400

    add second solution to uncrossedLines using matrix grid; refactor to pass all test cases

--- leet/30DaysOfCodePt2/uncrossedLines.js
+++ leet/30DaysOfCodePt2/uncrossedLines.js
@@ -13,7 +13,7 @@
 // Return the maximum number of connecting lines we can draw in this way.
 
 // * first attempt:
-const maxUncrossedLines = (A, B) => {
+const maxUncrossedLines1 = (A, B) => {
   // declare two pointer variables to keep track of length
   let a = A.length;
   let b = B.length;
@@ -30,3 +30,31 @@ const maxUncrossedLines = (A, B) => {
   };
   return maxUncrossedHelper();
 };
+
+// * second attempt:
+const maxUncrossedLines = (A, B) => {
+  // quick edge case check/base case
+  if(A === null || B === null || A.length === 0 || B.length === 0) {
+    return 0;
+  }
+
+  // create dp matrix to store values
+  const dp = new Array(A.length + 1, B.length + 1);
+
+  // iterate through A's values/subgrids -- aka the rows
+  for(let i = 1; i <= A.length; i += 1) {
+    // loop through B's values -- aka the cols
+    for(let k = 1; k <= B.length; k += 1) {
+      // check if the current element in A is equal to B
+      if(A[i - 1] === B[k - 1]) {
+        // reassign the i and k values
+        dp[i][k] = 1 + dp[i - 1][k - 1];
+      } else {
+        dp[i][k] = Math.max(dp[i][k - 1], dp[i - 1][k]);
+      }
+    }
+  }
+  return dp[A.length][B.length];
+}
+
+// 
