a72b3ac25f19316dc9cf0676cff38757be9c0f37
rllzrc <rellas.email@gmail.com>
2020-05-26 18:12:06 -0400

    update algoAvatar folder with additonal practice: LCS using hash table solution; refactor to pass all test cases

---
+++ algoAvatar/longestCommonSubsequence.js
@@ -0,0 +1,51 @@
+// * Get Your Reps In ~ !!
+// * Longest Common Subsequence aka Longest Substring Without Dupes ~
+
+// build hash table to keep track of character frequencies
+// create a variable for a start index value
+// create another variable to build the longest subsequence
+// if character already exists - update the start index value
+// start index = max of start index (current) + position of the last current letter (index) + 1
+// this will be our new potential substring start pointer
+// in the hash table, update the last seen value of the character index
+
+// * first attempt: use hash table / memoization
+// time complexity:
+// * O(n) -> constant -> n is the length of the input string
+// space complexity:
+// * O(min(n,a)) -> the minimum value of the characters frequency in compared to the number of letters in the alphabet (a = length of alphabet represented by input string uniquely)
+
+const longestSubstringWithoutDuplication = string => {
+  // initiate a hash table variable
+  const cache = {};
+  // longest substring result arr
+  let longest = [0, 1];
+  // declare a start index value
+  let startIndex = 0;
+
+  // iterate through the string
+  for(let i = 0; i < string.length; i += 1) {
+    // check if current character already exists in cache, update startIndex value
+    if(cache[string[i]]) {
+      startIndex = Math.max(startIndex, cache[i] + 1);
+    }
+    
+    // compute the difference, i + 1 to increase current position to the next
+    // if it is smaller, then we update longest value
+    if(longest[1] - longest[0] < i + 1 - startIndex) {
+      longest = [startIndex, i + 1]
+    }
+
+    // update cache table
+    // * PRO TIP!!
+    // this will take care of both cases:
+    // either we've already seen it or haven't, no matter what it adds or overwrites it in our hash table
+    cache[string[i]] = i;
+  }
+
+  return string.slice(longest[0], longest[1]);
+}
+
+// * test cases !!
+console.log(longestSubstringWithoutDuplication('clementisacap')); // ->
+console.log(longestSubstringWithoutDuplication('abcdeabcdefc')); // ->
