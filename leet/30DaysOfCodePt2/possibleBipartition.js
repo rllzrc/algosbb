6528ec48ca7b36d5784bc1a6c643565ce0ba6b60
rllzrc <rellas.email@gmail.com>
2020-05-27 23:57:54 -0400

    add second solution to possible bipartition, refactor and add comments for readability

--- leet/30DaysOfCodePt2/possibleBipartition.js
+++ leet/30DaysOfCodePt2/possibleBipartition.js
@@ -10,7 +10,7 @@
 
 // Return true if and only if it is possible to split everyone into two groups in this way.
 
-// * first attempt:m use DFS
+// * first attempt: use DFS
 const possibleBipartition = (N, dislikes) => {
   // quick edge case check:
   if(dislikes.length === 0) return true;
@@ -62,6 +62,43 @@ const possibleBipartition = (N, dislikes) => {
   }
   return true;
 }
+
+// * second attempt: iterative solution:
+const possibleBipartition = (N, dislikes) => {
+  const graph = new Array(N+1);
+  const color = {};
+  
+  const dfs = (node, c) => {
+      if (color[node]) {
+          return color[node] === c
+      }
+      color[node] = c;
+      
+      for(let i=0; i<graph[node].length; i++){
+          if (!dfs(graph[node][i], c ^ 1)) {
+              return false;
+          }
+      }
+      return true
+  }
+  
+  for(let i=1; i<=N; i++){
+      graph[i] = [];
+  }
+  
+  for(let i=0; i<dislikes.length; i++){
+      const edge = dislikes[i];
+      graph[edge[0]].push(edge[1]);
+      graph[edge[1]].push(edge[0]);
+  }
+  
+  for(let i=1; i<=N; i++){
+      if (color[i] == null && !dfs(i, 0)) {
+          return false
+      }
+  }
+  return true;
+};
 
 // * test cases!!
 console.log(possibleBipartition(N = 3, dislikes = [[1,2],[1,3],[2,3]])); // -> true, group1 [1,4], group2 [2,3]
