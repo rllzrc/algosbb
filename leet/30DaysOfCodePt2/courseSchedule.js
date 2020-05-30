6b80c4e70175dc502741cfd989eaa79d47a50352
rllzrc <rellas.email@gmail.com>
2020-05-30 00:22:47 -0400

    fix bug in canFinish func in courseSchedule file in regards to cannot access var before initialization; refactor to pass all test cases

--- leet/30DaysOfCodePt2/courseSchedule.js
+++ leet/30DaysOfCodePt2/courseSchedule.js
@@ -14,9 +14,10 @@ const canFinish = (numCourses, prerequisites) => {
   const cacheReqs = {};
   // create a new set to store all the courses that are completed
   const completed = new Set();
+  // create a new set variable to store all the courses that are currently being taken
+  const inProgress = new Set();
 
-
-  // iterate through the prereqs for each class and add it to its respective list; map out each item into the cacheReqs object
+  // iterate through the prereqs for each class and add it to its respective list; map out each item into the cacheReqs object -> gather all the prereqs for each class into an adjacency list
   for(let [a, b] of prerequisites) {
     if(!cacheReqs[a]) {
       cacheReqs[a] = [b];
@@ -33,41 +34,42 @@ const canFinish = (numCourses, prerequisites) => {
   }
 
   // for each course with a prereq check if a cycle exists
-  for(let course in prereq) {
+  for(let course in cacheReqs) {
     if(checkCycle(course)) {
       return false;
     }
+    // if no cycles are found, we can complete the curriculum yaay!
+    return true;
   }
 
-  // if no cycles are found, we can complete the curriculum yaay!
-  return true;
-}
 
-// create a helper function starts with a course and will recursively call it again on each of that courses' respective prereqs
-// each time we visit a new course we mark it into the completed set
-// if at some point in the recursive call we find a course that is already marked as completed, we found a cycle! therefore we are unable to finish the curriculum
 
-// once all prereqs are successfully taken for a given course we also mark it as completed
-// if all courses are marked as completed, then we are able to finish the curriculum!
-const checkCycle = course => {
-  // create a new set variable to store all the courses that are currently being taken
-  const inProgress = new Set();
-  // check if the we've already completed the course, thus we don't need to worry about this one: return false!
-  if(completed.has(course)) return false;
-  // check if the current course is part of the inProgress list, thus there is a cycle!
-  if(inProgress.has(course)) return true;
-  // add the current course to the inProgress track
-  inProgress.add(course);
-  // for each prereq in the current course, recusively call the checkCycle function once again 
-  for(let i of cacheReq[course]) {
-    // if found, then return true
-    if(cacheReq(i)) return true;
-    // add it to the completed set
-    completed.add(course);
-    // return false if no cycles are found
-    return false;
+  // create a helper function starts with a course and will recursively call it again on each of that courses' respective prereqs
+  // each time we visit a new course we mark it into the completed set
+  // if at some point in the recursive call we find a course that is already marked as completed, we found a cycle! therefore we are unable to finish the curriculum
+
+  // once all prereqs are successfully taken for a given course we also mark it as completed
+  // if all courses are marked as completed, then we are able to finish the curriculum!
+  function checkCycle(course) {
+    // check if the we've already completed the course, thus we don't need to worry about this one: return false!
+    if(completed.has(course)) return false;
+    // check if the current course is part of the inProgress list, thus there is a cycle!
+    if(inProgress.has(course)) return true;
+    // add the current course to the inProgress track
+    inProgress.add(course);
+    // for each prereq in the current course, recusively call the checkCycle function once again 
+    for(let i of cacheReqs[course]) {
+      // if found, then return true
+      if(checkCycle(i)) return true;
+      // add it to the completed set
+      completed.add(course);
+      // return false if no cycles are found
+      return false;
+    }
   }
-}
+};
+
+
 
 // * test cases:
 console.log(canFinish(numCourses = 2, prerequisites = [[1,0]])); // -> true
