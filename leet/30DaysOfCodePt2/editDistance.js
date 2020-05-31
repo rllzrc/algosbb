96f227608ef6d4837bf35c58181a49ba1b160370
rllzrc <rellas.email@gmail.com>
2020-05-31 19:40:55 -0400

    add base file of editDistance challenge

---
+++ leet/30DaysOfCodePt2/editDistance.js
@@ -0,0 +1,21 @@
+// ** Day 31 of 30 Days of Code Challenge Part ii -- May Edition! 
+// ** --> {  Edit Distance ! }
+
+// T A S K !!!
+
+// Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+// You have the following 3 operations permitted on a word:
+
+// Insert a character
+// Delete a character
+// Replace a character
+
+// * first attempt:
+const minDistance = (word1, word2) => {
+  
+}
+
+// * test cases !!
+console.log(minDistance(word1 = "horse", word2 = "ros")); // -> 3
+console.log(minDistance(word1 = "intention", word2 = "execution")); // -> 5
