36c5f72ecfcb9eb2358549f5842bf076a5cd0a47
rllzrc <rellas.email@gmail.com>
2020-06-07 22:34:43 -0400

    add second solution to coinChange2using dp arr and forEach

--- leet/30DaysOfCodePt3/coinChange2.js
+++ leet/30DaysOfCodePt3/coinChange2.js
@@ -5,7 +5,7 @@
 // You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
 
 // * first attempt: using DP
-const change = (amount, coins) => {
+const change1 = (amount, coins) => {
   // create a dp array with size of amount + 1
   const dp = new Array(amount + 1).fill(0);
   //fill in 1 for 0th index
@@ -14,14 +14,36 @@ const change = (amount, coins) => {
   // loop through arr of coins
   for(let i = 0; i < coins.length; i += 1) {
     for(let k = coins[i]; k <= amount; k += 1) {
+      // add combos and add to previous version of combination
       const remainder = k - coins[i];
       dp[k] += dp[remainder];
+      //console.log('dp:', dp);
     }
   }
   return dp[amount];
 }
 
+// * second attempt: using DP and forEach
+const change = (amount, coins) => {
+  // create a matrix grid with size of the amount + 1
+  const dp = new Array(amount + 1).fill(0);
+  // default amount fill in one for zero index
+  dp[0] = 1;
+
+  // loop through coins
+  coins.forEach(coin => {
+    for(let i = coin; i < dp.length; i += 1) {
+      // reassign values
+      console.log('index:', dp[i]);
+      console.log('el:', coin);
+      console.log('matrix:', dp);
+      dp[i] = dp[i] + dp[i - coin];
+    }
+  });
+  console.log('dp:', dp);
+  return dp[dp.length - 1];
+}
 // * test cases !!
 console.log(change(amount = 5, coins = [1, 2, 5])); // -> 4
-console.log(change(amount = 3, coins = [2])); // -> 0
-console.log(change(amount = 10, coins = [10])); // -> 1
+//console.log(change(amount = 3, coins = [2])); // -> 0
+//console.log(change(amount = 10, coins = [10])); // -> 1
