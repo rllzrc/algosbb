4ed64e81e68319e22c03e4637f422fa8a8de7f80
rllzrc <rellas.email@gmail.com>
2020-06-08 22:01:59 -0400

    add second solution to uniquePaths challenge using dp; passes all test cases

--- leet/putYourRepsIn/uniquePaths.js
+++ leet/putYourRepsIn/uniquePaths.js
@@ -9,7 +9,7 @@
 // How many possible unique paths are there?
 
 // * first attempt: using DP! ~
-const uniquePaths = (m, n) => {
+const uniquePaths1 = (m, n) => {
   // construct dp grid/matrix
   const dp = [];
   // fill in cells within dp 
@@ -28,6 +28,32 @@ const uniquePaths = (m, n) => {
   return dp[n - 1][m - 1];
 }
 
+// * second attempt: still using DP
+const uniquePaths = (m, n) => {
+  // create a variable to construct grid/matrix dp
+  const dp = [];
+  // fill in row and cols
+  // first outer loop will be the rows
+  for(let i = 0; i < m; i += 1) {
+    dp.push(new Array(m).fill(1));
+  }
+
+  // set row and col to be 1 to create the buffer
+  dp[0] = Array(n).fill(1);
+
+  // build out the row and cols, the # of paths is = to the paths' cell above and to the left.
+  for(let i = 1; i < dp.length; i += 1) {
+    for(let k = 1; k < dp[0].length; k += 1) {
+      dp[i][k] = dp[i - 1][k] + dp[i][k - 1];
+      //console.log('dp@0:', dp[0]);
+    }
+  }
+  console.log(dp);
+  return dp[m - 1][n - 1];
+}
+
+// 
+
 // * test cases!!
-console.log(uniquePaths(m = 3, n = 2)); // -> 3
+//console.log(uniquePaths(m = 3, n = 2)); // -> 3
 console.log(uniquePaths(m = 7, n = 3)); // -> 28
