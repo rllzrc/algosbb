39c065720dc6103366149c22bce992f04a2448aa
Rella <rellas.email@gmail.com>
2020-06-10 14:14:03 -0400

    add searchInsertPosition challenge first solution using binary search O(log n) time complexity

--- leet/30DaysOfCodePt3/searchInsertPosition.js
+++ leet/30DaysOfCodePt3/searchInsertPosition.js
@@ -6,9 +6,39 @@
 
 // You may assume no duplicates in the array.
 
-// * first attempt: brute force/naive
+// * first attempt: use binary search
+// search if target is in the given array
+// if not found, search for position to insert
+// have two pointer variables L = 0 R = last index -> mid = L + R / 2
+// compare if value of mid is = to target, return index ya found it!
+// if not equal to mid, search left or right -> modify mid - 1 for right -> mid + 1 if left side
+// if target not found, LR collides
+// L R M collides -> at this point, we stop, return pointer (left) to insert target
 const searchInsert = (nums, target) => {
-
+  // create pointer variables to perform binary search
+  let left = 0;
+  let right = nums.length-1;
+  
+  // iterate while left is less than or equal to right value
+  while(left <= right) {
+    // create a mid pointer 
+    let mid = Math.floor((left + right) / 2);
+    // check if mid is equal to target, if so return mid
+    if(nums[mid] === target) {
+      return mid;
+    }
+    // if target is less than nums at mid
+    // reassign pointer variables to move on the left side of the arr
+    if(target < nums[mid]) {
+      right = mid - 1;
+    } else {
+      // if it is greater, move pointer to be on the right side of the arr
+      left = mid + 1;
+    }
+  }
+  // if target is not found in the array, L & R values will eventually collide
+  // at this point, we stop and return left to insert the target
+  return left;
 }
 
 // * test cases!!
