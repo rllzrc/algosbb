b11727c9530fd96ba622428e273b87fd0717ca80
rllzrc <rellas.email@gmail.com>
2020-06-10 11:52:05 -0400

    add serachInsertPosition base file; refactor to pass all test cases with brute force solution

---
+++ leet/30DaysOfCodePt3/searchInsertPosition.js
@@ -0,0 +1,18 @@
+// ** Day 7 of 30 Days of Code Challenge Part iii -- June Edition! 
+// ** --> { Search Insert Position }
+
+// T A S K !!
+// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You may assume no duplicates in the array.
+
+// * first attempt: brute force/naive
+const searchInsert = (nums, target) => {
+
+}
+
+// * test cases!!
+console.log(searchInsert([1,3,5,6], 5)); // -> 2
+console.log(searchInsert([1,3,5,6], 2)); // -> 1
+console.log(searchInsert([1,3,5,6], 7)); // -> 4
+console.log(searchInsert([1,3,5,6], 0)); // -> 0
