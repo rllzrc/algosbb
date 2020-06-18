bb412c36cc23ab853c2f69943beb432f53f96125
rllzrc <rellas.email@gmail.com>
2020-06-18 15:04:19 -0400

    add additional test case for H-IndexII challenge

--- leet/30DaysOfCodePt3/H-IndexII.js
+++ leet/30DaysOfCodePt3/H-IndexII.js
@@ -63,5 +63,6 @@ const hIndex = citations => {
 
 // * test case!!
 console.log(hIndex(citations = [0,1,3,5,6])); // -> 3 [0,1,3,5,6] means the researcher has 5 papers in total and each of them had received 0, 1, 3, 5, 6 citations respectively. Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, her h-index is 3.
+console.log(hIndex(citations = [0,1,4,5,7]));
 
 
