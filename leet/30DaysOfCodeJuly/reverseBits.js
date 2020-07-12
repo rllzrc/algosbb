c4deeabeeb484c2911e9acf145bc6a5aa03eed14
rllzrc <rellas.email@gmail.com>
2020-07-12 11:09:04 -0400

    add second attempt at reverseBits challenge with clenaer code

--- leet/30DaysOfCodeJuly/reverseBits.js
+++ leet/30DaysOfCodeJuly/reverseBits.js
@@ -5,7 +5,7 @@
 // Reverse bits of a given 32 bits unsigned integer.
 
 // * first attempt: iteratively
-const reverseBits = n => {
+const reverseBits1 = n => {
   // create a sum variable
   let sum = 0;
   for(let i = 0; i < 32; i += 1) {
@@ -16,6 +16,19 @@ const reverseBits = n => {
   }
   return sum;
 }
+
+
+var reverseBits = function(n) {
+  var result = 0;
+  var count = 32;
+
+  while (count--) {
+    result *= 2;
+    result += n & 1;
+    n = n >> 1;
+  }
+  return result;
+};
 
 // * test cases!!
 console.log(reverseBits(00000010100101000001111010011100)); // -> 00111001011110000010100101000000
