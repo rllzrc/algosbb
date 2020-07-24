7dec55d8c96a75466746038a1875186a6cfc7ee3
rllzrc <rellas.email@gmail.com>
2020-07-23 23:14:33 -0400

    add BFS second solution to zigzagBinaryTreeLvlOrderTraversal; time limit exceeded refactor to pass all test cases

--- leet/30DaysOfCodeJuly/binaryTreeZigzagLevelOrderTraversal.js
+++ leet/30DaysOfCodeJuly/binaryTreeZigzagLevelOrderTraversal.js
@@ -37,7 +37,7 @@
 
 // if level % 2 === 1, append the root.val
 // if level % 2 === 0, inseart root.val at index[0]
-const zigzagLevelOrder = root => {
+const zigzagLevelOrder1 = root => {
   // base case
   if(!root) return [];
   // create a result variable to store and return value out later
@@ -70,3 +70,47 @@ function zigzagHelper(root, level, result) {
   zigzagHelper(root.right, level + 1, result);
   zigzagHelper(root.left, level + 1, result);
 }
+
+// * secong attempt: clean up above code, using BFS
+// run two level nested loop, outer iterates each level on the tree and inner iterates on each node within second level
+// single loop: append nodes to be visited into a queue DS and separate nodes with some sort of delimiter
+// left to right: use FIFO
+// append new tail to the queue so that the element that comes later will be in the proper position 
+const zigzagLevelOrder = root => {
+  // base case
+  if(!root) return [];
+  // create a result variable 
+  const result = [];
+  // create a depth variable to keep track of where we are at
+  let depth = 0;
+  // create a queue data structure to keep track of levels
+  let queue = [root];
+  // iterate through while queue has elements in it
+  while(queue.length > 0) {
+    // create a variable to hold on to sz of queue
+    const size = queue.length; 
+    // create a level variable to keep track
+    const level = [];
+
+    // iterate through queue 
+    for(let i = 0; i < size; i += 1) {
+      const node = queue.shift();
+      // if the depth we are at is divisible by 2, push the current node to level
+      if(depth % 2 === 0) {
+        level.push(node.val);
+      } else {
+        // unshift and put it in the front of the level array
+        level.unshift(node.val);
+      }
+
+      // check the children nodes
+      if(root.left) queue.push(root.left);
+      if(root.right) queue.push(root.right);
+    }
+
+    result.push(level);
+    // increment depth after each level 
+    depth += 1;
+  }
+  return result;
+};
