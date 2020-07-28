52bf2a7a39abe0f397252720c68cc66a070c1e25
rllzrc <rellas.email@gmail.com>
2020-07-28 14:25:51 -0400

    add second attempt at same frequency counter solution using map; O(n)

--- dailyChallenges/algosDSMasterclass/same.js
+++ dailyChallenges/algosDSMasterclass/same.js
@@ -5,6 +5,7 @@
 // Check if two arrays have the same values when squared
 
 // * naive/brute force approach:
+// * runtime = quadratic O(N^2)
 const same1 = (arr1, arr2) => {
   // quick edge case / short circuit check
   if(arr1.length !== arr2.length) {
@@ -25,3 +26,35 @@ const same1 = (arr1, arr2) => {
 
 // * test case!
 console.log(same1([1,2,3,2], [9,1,4,4])); // -> true
+
+// * second approach: refactored with frequency counter maps
+const same2 = (arr1, arr2) => {
+  // quick short circuit check:
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // create variables to store frequency counters:
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  // map out key/value pairs
+  for(let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for(let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  // iterate through each frequency map counter
+  for(let key in frequencyCounter1) {
+    if(!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// * test case!
+console.log(same2([1,2,3,2], [9,1,4,4])); // -> true
