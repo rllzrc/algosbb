5791a8b0fae0255a333dc9b11755a8ae61e76424
rllzrc <rellas.email@gmail.com>
2020-08-01 17:43:23 -0400

    add second solution to detectCapital challenge; easier to read O(1) runtime with more test cases

--- leet/30DaysOfCodeAug/detectCapital.js
+++ leet/30DaysOfCodeAug/detectCapital.js
@@ -17,6 +17,23 @@ const detectCapitalUse = word => {
   return word === word.toUpperCase() || word === word[0] + word.substr(1).toLowerCase();
 };
 
+// * second attempt: easier to read
+// time complexity:
+// * Constant --> O(1)
+const detectCapitalUse = word => {
+  // check if all caps
+  if(word.toUpperCase() === word) return true;
+  // check if all lowercase
+  if(word.toLowerCase() === word) return true;
+  // check first character is caps only
+  if(word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase() === word) return true;
+  // else 
+  return false;
+}
+
 // * test cases
 console.log(detectCapitalUse('FlaG')); // -> false
 console.log(detectCapitalUse('Google')); // -> true
+console.log(detectCapitalUse('apple')); // -> true
+console.log(detectCapitalUse('TACO')); // -> true
+console.log(detectCapitalUse('TAcooO')); // -> false
