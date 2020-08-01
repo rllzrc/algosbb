19d7e6fbf3161e162102d175af524eed97313ed7
rllzrc <rellas.email@gmail.com>
2020-08-01 18:14:46 -0400

    add additional solutions to areThereDupes challenge using pointer approach (refactor) and one-line Set solution

--- dailyChallenges/algosDSMasterclass/patternsChallenge.js
+++ dailyChallenges/algosDSMasterclass/patternsChallenge.js
@@ -45,7 +45,7 @@ const sameFrequency = (num1, num2) => {
 // Implement a function called areThereDuplicates which accepts a variable number of args and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
 
 // * first attempt: use frequency counter 
-function areThereDuplicates() {
+function areThereDuplicates1 () {
   // create a cache to store frequency counts
   const cache = {};
   // iterate through values in arguments
@@ -61,6 +61,31 @@ function areThereDuplicates() {
   // else
   return false;
 };
+
+// * second attempt: multiple pointers
+const areThereDuplicates2 = (...args) => {
+  // sort out the args arr
+  args.sort((a, b) => a > b);
+  // create pointer variables
+  let left = 0;
+  let right = 1;
+  // iterate through while right is less than the length of args
+  while(right < args.length) {
+    // check if args at left is equal to right
+    if(args[left] === args[right]) {
+      return true;
+    }
+    // increment both values to move on the the next element
+    left += 1;
+    right += 1;
+  }
+  return false;
+};
+
+// * third attempt: one liner using set
+function areThereDuplicates3 () {
+  return new Set(arguments).size !== arguments.length;
+}
 
 // * test cases!!
 console.log(areThereDuplicates(1,2,3)); // -> false
