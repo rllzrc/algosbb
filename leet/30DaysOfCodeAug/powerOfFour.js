7f571b74f16f903e69267035c768c36636f31ed1
rllzrc <rellas.email@gmail.com>
2020-08-04 13:36:24 -0400

    add 30Days challenge powerOfFour with first attempt at solution using % and division

---
+++ leet/30DaysOfCodeAug/powerOfFour.js
@@ -0,0 +1,27 @@
+// ** Day 4 of 30 Days of Code Challenge Part X -- August Edition! 
+// ** --> { Power of Four ! }
+
+// T A S K !!
+// Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+// * first attempt:
+// take log of given num on base 4, if positive integer then true
+// or keep dividing the num by 4, if result is non zero and not 1 then false
+const isPowerOfFour = num => {
+  // quick edge case check
+  if(num === 0) return 0;
+  // iterate while n is not 1
+  while(num !== 1) {
+    // perform division
+    if(num % 4 !== 0) {
+      return false;
+    }
+    // keep dividing by 4
+    num = num / 4;
+  }
+  return true;
+}
+
+// * test cases!!
+console.log(isPowerOfFour(16)); // -> true
+console.log(isPowerOfFour(5)); // --> false
