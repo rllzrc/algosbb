31dd423c6f6634eeb8506018c5444e05f37c6321
rllzrc <rellas.email@gmail.com>
2020-08-04 17:22:51 -0400

    add helper and pure recursion examples to dailyChallenges folder

--- dailyChallenges/algosDSMasterclass/Recursion/exampleRecursion.js
+++ dailyChallenges/algosDSMasterclass/Recursion/exampleRecursion.js
@@ -17,3 +17,61 @@ const sumRange = num => {
   if(num === 1) return 1;
   return num + sumRange(num - 1);
 }
+
+// * write a factorial iteratively
+// 4! = 4 * 3 * 2 * 1
+const factorial = num => {
+  let total = 1;
+  for(let i = num; i > 1; i -= 1) {
+    total *= i;
+  }
+  return total;
+}
+
+// * recurisve factorial ~
+const factorial2 = num => {
+  // base case
+  if(num === 1) return 1;
+  return num * factorial(num - 1);
+};
+
+// * helper method recursion pattern
+const outer = input => {
+  let outerScopedVar = [];
+  function helper(helperInput) {
+    // modify the outerScopedVar
+    helper(helperInput--);
+  }
+  helper(input);
+  return outerScopedVar;
+}
+
+// a more concrete example:
+const collectOddValues = arr => {
+  let result = [];
+
+  function helper(helperInput) {
+    if(helperInput.length === 0) {
+      return;
+    }
+
+    if(helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
+
+    helper(helperInput.slice(1));
+  }
+  helper(arr);
+  return result;
+}
+
+// using only pure recursion:
+const collectOddValuesPure = arr => {
+  let newArr = [];
+  if(arr.length === 0) return newArr;
+  if(arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+  newArr = newArr.concat(collectOddValuesPure(arr.slice(1)));
+  return newArr;
+}
