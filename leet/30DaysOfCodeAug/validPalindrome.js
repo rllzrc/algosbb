cc05f07b68f58fc38cf1b90ae1db5c2311ba7e10
rllzrc <rellas.email@gmail.com>
2020-08-03 23:12:08 -0400

    add second attemp at validPalindrome using regex; refactor to pass all test cases

--- leet/30DaysOfCodeAug/validPalindrome.js
+++ leet/30DaysOfCodeAug/validPalindrome.js
@@ -8,19 +8,19 @@
 // Note: For the purpose of this problem, we define empty string as valid palindrome.
 
 // * first attempt:
-const isPalindrome = str => {
+const isPalindrome = s => {
   // quick edge case check
-  if(str.length === 0) return true;
-  const regex = /\W/gm;
+  if(s.length === 0) return true;
   // sanitize str 
-  const sanitized = [...str.toLowerCase().replace(regex, '')];
+  const regex = /\W/gm;
+  const clean = [...s.toLowerCase().replace(regex, "")]
   // create pointer variables
   let left = 0;
-  let right = str.length-1;
+  let right = clean.length - 1;
   // while i and j havent met
   while(left < right) {
     // check if not the same
-    if(sanitized[left] !== sanitized[right]) {
+    if(clean[left] !== clean[right]) {
       return false;
     }
 
