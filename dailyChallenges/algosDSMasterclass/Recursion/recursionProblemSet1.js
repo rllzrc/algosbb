5a7b7eb11f0c321ac2d9c682d17add5ba731e786
rllzrc <rellas.email@gmail.com>
2020-08-06 22:55:31 -0400

    update recursionProb set 1 with final solution to task # 5 - Fib

--- dailyChallenges/algosDSMasterclass/Recursion/recursionProblemSet1.js
+++ dailyChallenges/algosDSMasterclass/Recursion/recursionProblemSet1.js
@@ -71,3 +71,39 @@ const productOfArray = nums => {
 // * test cases!
 // console.log(productOfArray([1,2,3])); // -> 6
 // console.log(productOfArray([1,2,3,10])); // -> 60
+
+// -----
+
+// *** T A S K # 4 !!! ~~
+// Write a function which accepts a number and adds up all the numbers from 0 to the number passed to the function.
+
+// * first attempt:
+const recursiveRange = num => {
+  // base case
+  if(!num || num === 0) return 0; 
+  // run again with different input
+  return num + recursiveRange(num - 1);
+};
+
+// * test cases!!
+// console.log(recursiveRange(6)); // -> 21
+// console.log(recursiveRange(10)); // -> 55
+
+// -----
+
+// *** T A S K # 5 !!! ~~
+// Write a recursive function which accepts a number and returns the nth number in the Fib sequence. Fib = sequence of whole numbers 1,1,2,3,5,8 starts with 1 and 1 and where every number thereafter is equal to the sum of the previous two numbers.
+
+// * first attempt:
+const fib = num => {
+  // base case
+  if(num <= 2) return 1;
+  // run again but change input
+  return fib(num - 1) + fib(num - 2);
+};
+
+// * test cases!!
+// console.log(fib(4)); // -> 3
+// console.log(fib(10)); // -> 55
+// console.log(fib(28)); // -> 317811
+// console.log(fib(35)); // -> 9227465
