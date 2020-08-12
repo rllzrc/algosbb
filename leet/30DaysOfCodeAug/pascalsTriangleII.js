e14c3f6ec2d67a5f8e9843fd4decb42c82075b44
rllzrc <rellas.email@gmail.com>
2020-08-12 13:16:44 -0400

    add hIndex challenge to 30DaysofCode; refactor to add comments and clean up while loop

---
+++ leet/30DaysOfCodeAug/pascalsTriangleII.js
@@ -0,0 +1,24 @@
+// ** Day 12 of 30 Days of Code Challenge Part X -- August Edition! 
+// ** --> { Pascal's Triangle !!! }
+
+// T A S K !!
+// Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+
+// Note that the row index starts from 0.
+
+// * first attempt:
+const hIndex = citations => {
+  if(!rowIndex) return [1];
+    
+  let res = [1, 1]
+  
+  while(--rowIndex) {
+      const next = [];
+      
+      for(let i = 0; i < res.length-1; i++) {
+          next.push(res[i] + res[i+1]);
+      }
+      res = [1, ...next, 1];
+  }
+  return res;
+};
