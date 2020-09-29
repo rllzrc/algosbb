f7851f5d290a84c675757fe74efd06c5bc76d724
rllzrc <rellas.email@gmail.com>
2020-09-28 23:14:15 -0400

    refactor: work on second solution to sw approach to subArrPrdctLessThanK challenge

--- leet/30DaysOfCodeAug/subArrayProductLessThanK.js
+++ leet/30DaysOfCodeAug/subArrayProductLessThanK.js
@@ -7,35 +7,55 @@
 // Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
 
 // * first attempt:
+// const numSubarrayProductLessThanK = (nums, k) => {
+//   // edge case checks
+//   if(!nums || nums.length === 0 && !k || k > nums.length) return;
+
+//   // input: [10, 5, 2, 6] k = 100
+//   // output : 8
+//   // create a max variable to keep track of, set to -Infinity
+//   let max = -Infinity;
+//   // iterate through nums 
+//   for(let i = 0; i < nums.length - k + 1; i += 1) {
+//     // create a temp variable to keep track of
+//     let temp = 0; 
+//     // iterate up until k value
+//     // this will build / keep track of the length of the sliding window
+//     for(let j = 0; j < k; j += 1) {
+//       // reassign temp variable
+//       // find product of values in between the window
+//       temp *= nums[i + j];
+//     }
+//     // check if temp is greater than max
+//     // if so reassign values
+//     if(temp > max) {
+//       max = temp; 
+//     }
+
+//   }
+//   return max;
+// }
+
+// * second attempt: sliding window approach
 const numSubarrayProductLessThanK = (nums, k) => {
-  // edge case checks
-  if(!nums || nums.length === 0 && !k || k > nums.length) return;
-
-  // input: [10, 5, 2, 6] k = 100
-  // output : 8
-  // create a max variable to keep track of, set to -Infinity
-  let max = -Infinity;
-  // iterate through nums 
-  for(let i = 0; i < nums.length - k + 1; i += 1) {
-    // create a temp variable to keep track of
-    let temp = 0; 
-    // iterate up until k value
-    // this will build / keep track of the length of the sliding window
-    for(let j = 0; j < k; j += 1) {
-      // reassign temp variable
-      // find product of values in between the window
-      temp *= nums[i + j];
+  // edge case check
+  if(k <= 1) return 0;
+  // create variables to keep track of
+  let product = 1;
+  let max = 0;
+  let left = 0;
+  // iterate through nums, assign product's value with each element
+  for(let right = 0; right < nums.length; right += 1) {
+    product *= nums[right];
+    // check as long as product is less than k target value
+    while(product >= k) {
+      // divide product by values on the left side
+      product /= nums[left += 1]; // will cause an infinite loop without it
+      max += right - left + 1;
     }
-    // check if temp is greater than max
-    // if so reassign values
-    if(temp > max) {
-      max = temp; 
-    }
-
   }
   return max;
 }
-
 
 // * test cases!
 console.log(numSubarrayProductLessThanK(nums = [10, 5, 2, 6], k = 100)); // 8 -> The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
