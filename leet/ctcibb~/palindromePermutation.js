08525618094feedfa3d9a6d29dc956dd7bc8bde7
rllzrc <rellas.email@gmail.com>
2020-10-15 14:27:11 -0400

    build: Add palindromePerm challenge using charset/cache

--- leet/ctcibb~/palindromePermutation.js
+++ leet/ctcibb~/palindromePermutation.js
@@ -1,60 +1,28 @@
 // ** Days of Code Challenge Part X -- August Edition! 
-// ** --> { First Unique Character In A String !!! }
+// ** --> { Palindrome Permutation !!! }
 
 // T A S K !!
-// Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+// Given a string, determine if a permutation of the string could form a palindrome.
 
-// * first attempt:
-const firstUniqChar1 = s => {
-  // edge case check
-  if(!s || s.length === 0) return -1; 
-  // create a cache/hash set frequency counter using Map
-  const map = new Map(); 
-  // add key value/pairs
-  for(let char of s) {
-    map.has(char) ? map.set(char, map.get(char) + 1) : map.set(char, 1);
-  }
-
-  console.log(map);
-
-  // find the index
-  for(let i = 0; i < s.length; i += 1) {
-    if(map.get(s[i]) === 1) return i;
-  }
-  
-  return -1;
-}
-
-
-// * second attempt: using reg cache object
-const firstUniqChar = s => {
-  // edge case check
-  if(!s || s.length === 0) return -1;
-  // create a variable to return out result 
-  let result = -1;
-  // create a variable to store char frequency
+// * first attempt: using charset/cache
+const palindromePermutation = s => {
+  // edge case check:
+  if(!s || s.length === 0) return false;
+  // create a cache variable to store char frequencies
   const cache = {};
-  // map out key/val pairs in cache
-  for(let i = 0; i < s.length; i += 1) {
-    // check if it doesnt exist in cache, add it, else increase value by 1
-    // create a variable to store current char
-    let curr = s[i];
-    if(!cache[curr]) {
-      cache[curr] = 1;
+  // map out key/val pairs ... if the letter already exists, delete it, if not add 1 to its value
+  for(let char of s) {
+    if(cache[char]) {
+      delete cache[char];
     } else {
-      cache[curr] += 1; 
+      cache[char] = 1
     }
   }
-  //console.log(cache);
-  // loop through cache and s + check which value has the first 1
-  for(let i = 0; i < s.length; i += 1) {
-    if(cache[s[i]] === 1) {
-      return i;
-    }
-  }
-  return result; 
-}
+  return Object.keys(cache).length <= 1;
+};
 
-// * test cases
-console.log(firstUniqChar('leetcode')); // -> 0
-console.log(firstUniqChar('loveleetcode')); // -> 2
+// * test cases~
+console.log(palindromePermutation('code')); // -> false
+console.log(palindromePermutation('aab')); // -> true
+console.log(palindromePermutation('carerac')); // -> true
+
