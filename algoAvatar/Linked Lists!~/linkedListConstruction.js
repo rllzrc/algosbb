c48534a22a0db6116323387103afaf94ec7f1016
rllzrc <rellas.email@gmail.com>
2020-10-20 18:35:06 -0400

    fix: Add remove methods to LL class

--- algoAvatar/Linked Lists!~/linkedListConstruction.js
+++ algoAvatar/Linked Lists!~/linkedListConstruction.js
@@ -34,7 +34,51 @@ class DoublyLinkedList {
     while(node !== null && node.val !== val) node = node.next; 
     return node !== null;
   }
+
+  // * time: O(n) - linear
+  // * space: O(1) - constant
+  removeNodesWithValue(val) {
+    // start with the head, grab its value
+    let node = this.head;
+    // iterate while node is not null
+    while(node !== null) {
+      // create a variable to store node to remove
+      const nodeToRemove = node;
+      // reassign pointers
+      node = node.next;
+      // check if node's value is the value you want to remove
+      if(nodeToRemove.val === val) {
+        this.remove(nodeToRemove);
+      }
+    }
+  }
+
+  // * time: O(1) - constant
+  // * space: O(1) - constant
+  remove(node) {
+    // check if node is the head
+    if(node === this.head) this.head = this.head.next;
+    // check opposite, check if its the tail
+    if(node === this.tail) this.tail = this.tail.prev;
+    // invoke node bindings method
+    this.removeNodeBindings(node);
+  }
+
+  removeNodeBindings(node) {
+    // check node's prev value, make sure its not set to null and value doesnt equal it
+    if(node.prev !== null) {
+      node.prev.next = node.next;
+    }
+    // same thing but checking its previous val
+    if(node.next !== null) {
+      node.next.prev = node.prev;
+    }
+    // reassign prev and next values
+    node.prev = null;
+    node.next = null;
+  }
 }
+
 
 
 
