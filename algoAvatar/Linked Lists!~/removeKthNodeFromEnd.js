8449723700ec143039d699fbe27f9f0eef4c31e6
rllzrc <rellas.email@gmail.com>
2020-10-19 23:03:19 -0400

    build: Create new LL folder with first challenge base file

---
+++ algoAvatar/Linked Lists!~/removeKthNodeFromEnd.js
@@ -0,0 +1,9 @@
+// ** AE: Arrays Daily Reps Challenge  
+// ** --> { remove Kth node from end !!! }
+
+// T A SK !!!
+
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+// Follow up: Could you do this in one pass?
+
