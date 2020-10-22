d9f9768a5d09c124354f509fe185b81fe59ae34a
rllzrc <rellas.email@gmail.com>
2020-10-21 22:51:37 -0400

    build: Continue to build out LL implementation, add insertBefore method

--- algoAvatar/Linked Lists!~/linkedListConstruction.js
+++ algoAvatar/Linked Lists!~/linkedListConstruction.js
@@ -24,6 +24,24 @@ class DoublyLinkedList {
     this.head = null;
     this.tail = null;
   }
+  // * time: O(1) - constant
+  // * space: O(1) - constant
+  insertBefore(node, nodeToInsert) {
+    // check if node to insert is equal to the head/tail aka the only node on the list
+    if(nodeToInsert === this.head || nodeToInsert === tail) return;
+    // invoke remove method on node to insert as a defensive check to not duplicate
+    this.remove(nodeToInsert);
+    nodeToInsert.prev = node.prev;
+    nodeToInsert.next = node; 
+    // chcek tho see if node is/is not the head of the LL
+    if(node.prev === null) {
+      this.head = nodeToInsert;
+    } else {
+      // take node's prev value and update its next pointer to be nodeToInsert
+      node.prev.next = nodeToInsert
+    }
+    node.prev = nodeToInsert; 
+  }
 
   // * time: O(n) - linear
   // * space: O(1) - constant
