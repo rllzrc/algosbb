1589da8f5842ddd176aad5f66a34f212f6b1320e
rllzrc <rellas.email@gmail.com>
2021-01-07 12:39:56 -0800

    build: Add Doubly LL + Node class to solution; refactor to add methods to LRU

--- algoAvatar/Linked Lists!!/LRUCache.js
+++ algoAvatar/Linked Lists!!/LRUCache.js
@@ -17,6 +17,9 @@
 
 // Note that inserting a key-value pair with an already existing key should simply replace the key's value in the cache with the new value and shouldn't evict a key-value pair if the cache is full. Lastly, attempting to retrieve a value from a key that isn't in the cache should return null. 
 
+// * Time Complexity: methods should be all in constant time O(1)
+// * Space Complexity: methods should be all in constant time O(1)
+
 // LRU Cache Class:
 // this is an input class
 class LRUCache {
@@ -25,5 +28,65 @@ class LRUCache {
     this.maxSize = maxSize || 1;
     this.currentSize = 0;
     this.listOfMostRecent = new DoublyLinkedList();
+  }
+};
+
+
+// * Doubly Linked List Class
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  setHeadTo(node) {
+    if(this.head === node) {
+      return;
+    } else if(this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else if(this.head === this.tail) {
+      this.tail.prev = node;
+      this.head = node;
+      this.head.next = this.tail;
+    } else {
+      if(this.tail === node) this.removeTail();
+      node.removeBindings();
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node; 
+    }
+  }
+
+  removeTail() {
+    if(this.tail === null) return;
+    if(this.tail === this.head) {
+      this.head = null;
+      this.tail = null;
+      return; 
+    }
+    this.tail = this.tail.prev;
+    this.tail.next = null; 
+  }
+}
+
+// * Doubly Linked List Node class
+class DoublyLinkedListNode {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+
+  removeBindings() {
+    if(this.prev !== null) {
+      this.prev.next = this.next;
+    }
+    if(this.next !== null) {
+      this.next.prev = this.prev; 
+    }
+    this.prev = null;
+    this.next = null; 
   }
 }
