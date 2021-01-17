7e7dc8a48256bc3d28131d578ee39d0ef61a71f4
rllzrc <rellas.email@gmail.com>
2021-01-16 22:49:29 -0500

    build: Add 3rd solution to TNS challenge in array folder, space optimized

--- algoAvatar/Arrays~!/twoNumberSum.js
+++ algoAvatar/Arrays~!/twoNumberSum.js
@@ -20,14 +20,16 @@
 
 // iterative solution, first attempt:
 // time: O(N^2) -> nested for loop
-// space: O(1)
+// space: O(1) -> no extra space, just two variables for readability no hash table etc
 const twoNumberSum = (array, targetSum) => {
   for(let i = 0; i < array.length; i += 1) {
     const firstNum = array[i];
+    // iterate through the rest of the nums in the array
     for(let k = i + 1; k < array.length; k += 1) {
       const secondNum = array[k];
       // check if both equate to target value
       if(firstNum + secondNum === targetSum) {
+        // return out an array of those two values
         return [firstNum, secondNum];
       }
     }
@@ -38,18 +40,45 @@ const twoNumberSum = (array, targetSum) => {
 
 // * second attempt using cache
 // * time: O(N)
-// * space: O(N)
+// * space: O(N) -> storing values in hash table 
 const twoNumberSum2 = (array, targetSum) => {
+  // initialize hash table aka cache object
   const cache = {};
+  // loop through array 
   for(const num of array) {
-    // x + y = 10 or y = 10 - x 
+    // x + y = 10 or y = 10(target sum) - x 
     // if y or 'match' is in hash table / cache, return x and y
     const match = targetSum - num;
     if(match in cache) {
       return [match, num];
     } else {
-      // store it in the hash table 
+      // store current num in the hash table 
       cache[num] = true;
+    }
+  }
+  return []; 
+};
+
+// * third attempt via sorting array (quicksort, mergesort, etc) - O(N log N) time ~ O P T I M I Z E D ~
+// * time: O(nLog(n)) -> a bit iffy compared to above due to sort
+// * space: O(1) -> if you value space efficiency more than time 
+
+const twoNumberSum3 = (array, targetSum) => {
+  // sort out array
+  array.sort((a, b) => a - b);
+  // initialize pointer values -> left at start, right at end
+  let left = 0;
+  let right = array.length - 1;
+  while(left < right) {
+    const currentSum = array[left] + array[right];
+    if(currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if(currentSum < targetSum) {
+      // increase the left pointer
+      left += 1;
+    } else if(currentSum > targetSum) {
+      // move the right pointer to the left by one position
+      right -= 1; 
     }
   }
   return []; 
