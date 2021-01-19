d2abe7a847b3ceb4265f0a6694c40eeb608d49a2
rllzrc <rellas.email@gmail.com>
2021-01-18 22:15:37 -0500

    build: Add 3NumSum solution using pointer strategy w comments for readability

--- algoAvatar/Arrays~!/threeNumberSum.js
+++ algoAvatar/Arrays~!/threeNumberSum.js
@@ -11,11 +11,48 @@
 // input: array of integers + a number representing target sum 
 // output: array of triplets that sum up to the target sum
 // constraints: optimize
-// edge cases: 
+// edge cases:
 
-// * time complexity:
-// * space complexity: 
+// * main squeeze: 
+// *  
+
+// * time complexity: O(N^2) -> N = length of array >> iterating over main array at least once + additional while loop to reset pointers, add triplets, etc.
+// * space complexity: O(N) -> for output array, we might need to store every single # if it is a valid triplet thus bounded by this space complexity 
 
 const threeNumberSum = (array, targetSum) => {
-  
-}
+  // sort the input array 
+  array.sort((a, b) => a - b);
+  // declare output variable
+  const triplets = [];
+  // iterate through input array
+  // * why up until array.length-2?! we are looking for triplets, we always want to have 3 nums -> left + right + currentNum (i) pointer so our last currentNum (first num in triplets output array) has to be the third value from the end of the array (because we want to have at least two other values to try to sum it up with)
+  // i = currentNum 
+  for(let i = 0; i < array.length - 2; i += 1) {
+    // initialize left and right pointers
+    // left = the index just to the righ of i
+    let left = i + 1;
+    // right - the final index in the array
+    let right = array.length - 1;
+    // iterate while left and right are not overlapping or past each other - to ensure we're in bounds
+    while(left < right) {
+      const currentSum = array[i] + array[left] + array[right]
+      // check currentSum against targetSum
+      if(currentSum === targetSum) {
+        // we found a valid triplet!
+        // append to array
+        triplets.push([array[i], array[left], array[right]]);
+        // increment left because we know that if we only move one we will be dealing with a sum that is greater than our targetSum for sure
+        left += 1;
+        // same with right, dealing with sum that will def be less than target sum thus change both pointers
+        right -=1; 
+      } else if(currentSum < targetSum) {
+        // increment left ONLY to guarantee a greater current sum 
+        left += 1
+      } else if(currentSum > targetSum) {
+        // decrement right ONLY to guarantee a smaller current sum
+        right -= 1; 
+      }
+    }
+  }
+  return triplets; 
+};
