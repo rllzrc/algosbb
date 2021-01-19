9a524fa045c2edeb27f72f57d90654a0b778ed04
rllzrc <rellas.email@gmail.com>
2021-01-18 21:15:51 -0500

    build: Add base files to 3NumSum challenge

---
+++ algoAvatar/Arrays~!/threeNumberSum.js
@@ -0,0 +1,21 @@
+// ** AE: Arrays Reps Challenge  
+// ** --> { Three Number Sum !!! }
+
+// T A SK !!!
+
+// Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. The function should find all triplets in the array that sum up to the target sum and return a two-dimensional array of all these triplets. The numbers in each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect to the numnbers they hold.
+
+// If no three numbers sum up to the target sum, the function should return an empty array. 
+
+// * --- Roadmap --- *
+// input: array of integers + a number representing target sum 
+// output: array of triplets that sum up to the target sum
+// constraints: optimize
+// edge cases: 
+
+// * time complexity:
+// * space complexity: 
+
+const threeNumberSum = (array, targetSum) => {
+  
+}
