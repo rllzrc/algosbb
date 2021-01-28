e8f9bdbdd014802701519cb55174703d098ba7ae
rllzrc <rellas.email@gmail.com>
2021-01-27 22:44:33 -0500

    build: Add base files to revWrds challenge

---
+++ leet/@mcrsft/reverseWordsInAString.js
@@ -0,0 +1,29 @@
+// ** @mcrsft Arrays + Strings Reps Challenge  
+// ** --> { reverse string !!! }
+
+// T A SK !!!
+
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+// Return a string of the words in reverse order concatenated by a single space.
+
+// Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+// * --- Roadmap --- *
+// input: array of strings
+// output: array of strings in reverse 
+// constraints: no extra auxiliary space (for another array), modify the input array in place with O(1) extra memory.
+// edge cases: 
+
+// * main squeeze:
+// two pointer approach -> one at the beginning + at the end
+// iterate while left is < right
+// swap left with right index value
+// move left pointer 1 step to the right, then right pointer one step to the left
+
+// * time complexity: 
+// * space complexity: 
+
+const reverseWords = s => {
+  
+}
