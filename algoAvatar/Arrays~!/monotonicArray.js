53cf312d5f748704494b9bab02a09c49bdd84590
rllzrc <rellas.email@gmail.com>
2021-01-28 18:51:44 -0500

    build: Add base files to monotonicArr challenge

---
+++ algoAvatar/Arrays~!/monotonicArray.js
@@ -0,0 +1,28 @@
+// ** AE: Arrays Reps Challenge  
+// ** --> { Monotonic Array !!! }
+
+// T A SK !!!
+
+// Write a function that takes in an array of integers and returns a boolean representing whether the array is monotonic.
+
+// An array is said to be monotonic if its elements, from left to right, are entirely non-increasing or entirely non-decreasing. 
+
+// The elements aren't necessarily exclusively decreasing or increasing; they simply don't do either. 
+
+// Note that empty arrays and arrays of one element are monotonic. 
+
+// * --- Roadmap --- *
+// input:
+// output:
+// constraints: 
+// edge cases: 
+
+// * main squeeze:
+//
+
+// * time complexity: 
+// * space complexity: 
+
+
+
+
