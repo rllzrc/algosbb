b291251d9419f93b24f8f2ad1caf5541c622e7b2
rllzrc <rellas.email@gmail.com>
2021-02-04 12:30:17 -0500

    build: Add base files to kadanesAlgo challenge in aA folder

---
+++ algoAvatar/Arrays~!/kadanesAlgo.js
@@ -0,0 +1,26 @@
+// ** AE: Arrays Reps Challenge  
+// ** --> { Kadane's Algorithm !!! }
+
+// T A SK !!!
+
+// Write a function that takes in a non-empty array of integers and returns the maximum sum that can be obtaned by summing up all of the integers in a non-empty subarray of the input array. A subarray must only contain adjacent numbers (numbers next to each other in the input array). 
+
+
+// * --- Roadmap --- *
+// input: an array of integers 
+// output: integer (max sum) 
+// constraints: optimize 
+// edge cases: 
+
+// * main squeeze:
+
+// * time complexity: 
+// * space complexity:
+
+// * first attempt: 
+const kadanesAlgorithm = array => {
+  
+}
+
+
+
