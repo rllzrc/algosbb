8ad7ce80b422172b688bd837c16774c882846614
rllzrc <rellas.email@gmail.com>
2021-02-06 13:00:34 -0500

    build: Add base files for validParens challenge in m folder

---
+++ leet/@mcrsft/validParentheses.js
@@ -0,0 +1,28 @@
+// ** @mcrsft Arrays + Strings Reps Challenge  
+// ** --> { reverse string !!! }
+
+// T A SK !!!
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+
+// * --- Roadmap --- *
+// input: string of special characters (parentheses)
+// output: boolean 
+// constraints: optimize
+// edge cases: 
+
+// * main squeeze:
+
+
+// * time complexity: 
+// * space complexity: 
+
+// * first attempt:
+const isValid = s => {
+  
+}
