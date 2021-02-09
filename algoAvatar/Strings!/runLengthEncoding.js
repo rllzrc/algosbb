df88e04acec3f223369bcfdc30b4260cfadde600
rllzrc <rellas.email@gmail.com>
2021-02-09 11:14:01 -0500

    build: Add rnLngthEncdng base file challenges to Strings aA folder

---
+++ algoAvatar/Strings!/runLengthEncoding.js
@@ -0,0 +1,28 @@
+// ** AE: S T A C K S ~ REP Challenges! 
+// ** --> { Balanced Brackets !!! }
+
+// T A SK !!!
+
+// Write a function that takes in a non-empty string and returns its run-length encoding.
+
+// Uhh.. whut? Run-length encoding from wiki: a form of lossless data compression in which runs of data are stored as single data value and count, rather than as the original run. For this problem, a run data is any sequence of consecutive, identical characters. So the run "AAA" would be a run-length code of "3A".
+
+// Of course we like to make things more complicated when it comes to solving algos sooo.... the input can contain all sorts of special characters, including numbers. Since encoded data must be decodable, this means that we can't naively run-length-encode long runs. Thus, long runs (runs of 10 characters or more) should be encoded in a S P L I T fashion, such as "AAAAAAAAAAAA" => "9A3A"
+
+
+// * --- Roadmap --- *
+// input: a non empty string of chacracters (including numbers)
+// output: string in run-length encoding format
+// constraints: optimize 
+// edge cases: 
+
+// * main squeeze:
+
+
+// * time complexity: 
+// * space complexity: 
+
+// * first attempt: 
+const runLengthEncoding = string => {
+  
+}
