dad5b8cfb3f5a8439b6929ba9b7ea69a1d3c4ef0
rllzrc <rellas.email@gmail.com>
2021-02-08 23:18:30 -0500

    build: Add Stacks Reps Challenge folder w base files to first task - blncdBrckts

---
+++ algoAvatar/Stacks~!/balancedBrackets.js
@@ -0,0 +1,29 @@
+// ** AE: S T A C K S ~ REP Challenges! 
+// ** --> { Balanced Brackets !!! }
+
+// T A SK !!!
+
+// Write a function that takes in a string made up of brackets and other optional characters. The function should return a boolean representing whether the string is balanced with regards to brackets.
+
+// A string is said to be balanced if it has as many opening brackets of a certain type as it has closing brackets of that type and if no bracket is unmatched. Note that an opening bracket can't match a corresponding closing bracket that comes before it, and similarly, a closing bracket can't match a corresponding opening bracket that comes after it. Also, brackets can't overlap each other as in [(]).
+
+
+// * --- Roadmap --- *
+// input: a string of brackets + other optional characters
+// output: boolean
+// constraints: optimize 
+// edge cases: 
+
+// * main squeeze:
+
+// * time complexity: 
+// * space complexity: 
+
+// * first attempt: 
+const balancedBrackets = string => {
+
+}
+
+
+
+
