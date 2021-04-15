09277cf703941cf4be943b96ff6f067a9f138204
rllzrc <rellas.email@gmail.com>
2021-04-14 21:26:29 -0400

    fix: Adjust grpAnagrams sol in leet\m folder

--- leet/@mcrsft/groupAnagrams.cs
+++ leet/@mcrsft/groupAnagrams.cs
@@ -20,32 +20,19 @@ public class Solution {
     if(strs == null || strs.Length == 0) {
       return new List<IList<String>>();
     }
+
+		var map = new Dictionary<String, List<String>>();
+
+		foreach (String s in strs) {
+			char[] char = s.ToCharArray();
+			Array.Sort(char);
+
+			String keyStr = new string(char);
+			if(!map.ContainsKey(keyStr)) {
+				map.Add(keyStr, new List<String>());
+			}
+			map[keyStr].Add(s);
+		}
+		return new List<IList<String>>(map.Values);
   }
 }
-
-/*
-public class Solution {
-    public IList<IList<string>> GroupAnagrams(string[] strs) {
-         if(strs == null || strs.Length == 0){
-		        return new List<IList<String>>();
-	        }
-
-	        var map = new Dictionary<String, List<String>>();
-	        
-	        foreach (String s in strs) {
-		        char[] ca = s.ToCharArray();
-		        Array.Sort(ca);
-
-		        String keyStr = new string(ca);
-		        if(!map.ContainsKey(keyStr))
-                {
-			        map.Add(keyStr, new List<String>());
-                }
-
-		        map[keyStr].Add(s);
-	        }	       
-
-	        return new List<IList<String>>(map.Values);
-    }
-}
-*/ 
