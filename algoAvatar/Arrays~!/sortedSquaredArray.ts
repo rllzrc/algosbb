23a3432025fbcf70d8cc0bf450169e8a7e91a1e7
rllzrc <rellas.email@gmail.com>
2021-05-10 23:08:47 -0400

    build: Add TS solution to srtSq challenge in aa folder

---
+++ algoAvatar/Arrays~!/sortedSquaredArray.ts
@@ -0,0 +1,27 @@
+// ** AE: Arrays Reps Challenge  
+// ** --> { Sorted Squared Array !!! }
+
+// T A SK !!!
+
+// Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
+
+// * main squeeze:
+// brute force approach: traverse array, square each element, and if it contains negative numbers, sort it again prior to the return
+
+// * first attempt ~
+// * time complexity: O(nlogn) - due to additionally sorting 
+// * space complexity: O(N) N = length of the input array
+
+export function sortedSquaredArray(array: number[]) {
+  // create output array 
+  const sortedSquares = new Array(array.length).fill(0);
+
+  for(let i = 0; i < array.length; i += 1) {
+    const value = array[i];
+    // do some maths!
+    sortedSquares[i] = value * value;
+  }
+
+  sortedSquares.sort((a, b) => a - b);
+  return sortedSquares; 
+}
