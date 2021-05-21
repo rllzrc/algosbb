1c6c3424e637d6fe991cdbc375c0222400bdc3c8
rllzrc <rellas.email@gmail.com>
2021-05-20 23:41:49 -0400

    build: Add base files to 766 trnspsMtrx challenge

---
+++ leet/putYourRepsIn/867_transposeMatrix.js
@@ -0,0 +1,22 @@
+// ** Leetcode: Arrays Reps Challenge  ~ Easy Matrix Edition ~
+// ** --> { #867 Transpose Matrix }
+
+// T A SK !!!
+
+// Given a 2D integer array matrix, return the transpose of matrix.
+// The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+// * first attempt ~
+// * time complexity: 
+// * space complexity: 
+
+const transpose = matrix => {
+  // switch row and columns
+  // -1 [0,2] --> we moved: [2,0]
+
+  // iterate 2x --> outer loop is for rows and inner will be for columns (cells) 
+  // check the element' address (index value) then flip it
+  // push new address into output array
+  // at end return out
+  
+}
