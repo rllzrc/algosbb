782a0dd49d4d9377590bbefdfd4346b7cd39dae3
rllzrc <rellas.email@gmail.com>
2021-06-15 20:50:28 -0400

    build: Add base files for #206 challenge

---
+++ leet/putYourRepsIn/206_reverseLinkedList.js
@@ -0,0 +1,10 @@
+// ** Leetcode: Arrays Reps Challenge  ~ Linked List Edition ~
+// ** --> { #206 Reverse Linked List }
+
+// T A SK !!!
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// * first attempt ~
+// * time complexity: O(N)
+// * space complexity: O(N)
