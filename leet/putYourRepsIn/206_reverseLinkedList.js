47e5a77576bce490b79669d6670e4e5910047118
rllzrc <rellas.email@gmail.com>
2021-06-15 21:01:52 -0400

    build: Add solution to #206 challenge w comments for better understanding

--- leet/putYourRepsIn/206_reverseLinkedList.js
+++ leet/putYourRepsIn/206_reverseLinkedList.js
@@ -7,4 +7,26 @@
 
 // * first attempt ~
 // * time complexity: O(N)
-// * space complexity: O(N)
+// * space complexity: O(1)
+const reverseList = head => {
+  // initial instinct -> traverse entire list
+  // at each point, have another variable that stores the current value
+  // this.val and this.next are given properties
+  // so I want this.next to point to previous
+  // currentNode -> hold this value and next pointer
+  
+  // edge case check
+  if(!head) return null;
+  // declare pointer variables outside the while loop to ensure we can proceed accordingly
+  let previous = null;
+  let currentNode = head; 
+  while(currentNode) {
+    // currentNode = 1, next is still pointing to 2
+    let temp = currentNode.next // 2
+    // [1] -> 2 // prev -> null = <- 1 2 <- temp
+    currentNode.next = previous;
+    previous = currentNode;
+    currentNode = temp;
+  }
+  return previous;
+};
