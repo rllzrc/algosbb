4ab0ff33be5fa0b14de087af3105811ee9c4db05
rllzrc <rellas.email@gmail.com>
2021-06-17 00:03:07 -0400

    build: Add iterative solution to #235 challenge

--- leet/putYourRepsIn/235_LCAbSt.js
+++ leet/putYourRepsIn/235_LCAbSt.js
@@ -7,3 +7,30 @@
 
 // According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
+// * first attempt:
+// * time complexity: O(N)
+// * space complexity: O(1)
+
+const lowestCommonAncestor = (root, p, q) => {
+  let pVal = p.val;
+  let qVal = q.val;
+
+  const node = root;
+
+  while(node !== null) {
+    let parent = node.val;
+
+    if(pVal > parent && qVal > parent) {
+      // if both p and q are greater than parent, check right side
+      node = node.right;
+    } else if(pVal < parent && qVal < parent) {
+      // if botg are lesser, check right
+      node = node.left;
+    } else {
+      // we found the LCA here!
+      return node;
+    }
+  }
+  return null;
+}
+
