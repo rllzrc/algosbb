8e39bab51f97552401bf41e84ed8b06dd93a52ef
rllzrc <rellas.email@gmail.com>
2021-06-16 23:57:50 -0400

    build: Add base files for #235 challenge

---
+++ leet/putYourRepsIn/235_LCAbSt.js
@@ -0,0 +1,9 @@
+// ** Leetcode: Arrays Reps Challenge  ~ BST Edition ~
+// ** --> { #235 LCA of a Binary Search Tree }
+
+// T A SK !!!
+
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
